fda22f802cb5920bb24e3ba453381ff8ac73d460
alisher <alisher.nurmanov@inbox.ru>
2023-04-17 14:13:25 +0300

    removed old LICENSE remarks

--- cmd/edger/internal/states/schedule.go
+++ cmd/edger/internal/states/schedule.go
@@ -1,9 +1,6 @@
 /*
  * Copyright (c) 2021-present Sigma-Soft, Ltd.
  * @author: Nikolay Nikitin
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
  */
 
 package states
