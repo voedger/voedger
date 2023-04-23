37a98bb088ad2a350fa6e447c2f3e24fe16038e0
Michael Saigachenko <m.saigachenko@dev.untill.com>
2023-04-23 15:43:31 +0200

    pkg/parser: Naming conventions

--- pkg/parser/example_app/air.sql
+++ pkg/parser/example_app/air.sql
@@ -1,7 +1,7 @@
 SCHEMA air;
 
 IMPORT SCHEMA "github.com/untillpro/untill";
-IMPORT SCHEMA "github.com/untillpro/airsbp" AS air;		
+IMPORT SCHEMA "github.com/untillpro/airsbp" AS Air;
 
 COMMENT BackofficeComment "Backoffice Comment";
 TAG BackofficeTag;
@@ -14,59 +14,59 @@ FUNCTION MyTableValidator3(param1 aaa.TableRow, bbb.string) RETURNS ccc.TableRow
 ROLE UntillPaymentsUser;
 
 -- Comment for table
-TABLE air_table_plan OF CDOC (
-    fstate int,
-    name text NOT NULL,
-    vf text NOT NULL VERIFIABLE, -- Comment
-    i1 int DEFAULT 1,
-    s1 text DEFAULT "a",
-    ii int DEFAULT NEXTVAL(sequence),
-    id_bill int64 REFERENCES air.bill,
-    ckf text CHECK "^[0-9]{8}$",
+TABLE AirTablePlan OF CDOC (
+    FState int,
+    Name text NOT NULL,
+    VerifiableField text NOT NULL VERIFIABLE, -- Comment
+    Int1 int DEFAULT 1,
+    Text1 text DEFAULT "a",
+    Int2 int DEFAULT NEXTVAL(sequence),
+    BillID int64 REFERENCES air.bill,
+    CheckedField text CHECK "^[0-9]{8}$",
     UNIQUE fstate, name
 ) WITH Comment=BackofficeComment, Tags=[BackofficeTag];
 
-WORKSPACE ws (
+WORKSPACE MyWorkspace (
 
     COMMENT PosComment "Pos Comment";
     TAG PosTag;
 
-	USE TABLE somepackage.sometable;
-	USE TABLE mytable;
-	USE TABLE untill.*; 
+	USE TABLE SomeSchema.SomeTable;
+	USE TABLE AirTablePlan;
+	USE TABLE Untill.*; 
 
     ROLE LocationManager;
 
-    PROJECTOR ON COMMAND air.CreateUPProfile AS SomeFunc;
-    PROJECTOR ON COMMAND ARGUMENT untill.QNameOrders AS xyz.SomeFunc2;
-    PROJECTOR ON INSERT untill.bill AS SomeFunc;
-    PROJECTOR ON INSERT OR UPDATE untill.bill AS SomeFunc;
-    PROJECTOR ON UPDATE untill.bill AS SomeFunc;
-    PROJECTOR ON UPDATE OR INSERT untill.bill AS SomeFunc;
-    PROJECTOR ApplyUPProfile ON COMMAND IN (air.CreateUPProfile, air.UpdateUPProfile) AS air.FillUPProfile;
+    PROJECTOR ON COMMAND Air.CreateUPProfile AS SomeFunc;
+    PROJECTOR ON COMMAND ARGUMENT Untill.QNameOrders AS SomeSchema.SomeFunc2;
+    PROJECTOR ON INSERT Untill.Bill AS SomeFunc;
+    PROJECTOR ON INSERT OR UPDATE Untill.Bill AS SomeFunc;
+    PROJECTOR ON UPDATE Untill.Bill AS SomeFunc;
+    PROJECTOR ON UPDATE OR INSERT Untill.Bill AS SomeFunc;
+    PROJECTOR ApplyUPProfile ON COMMAND IN (Air.CreateUPProfile, Air.UpdateUPProfile) AS Air.FillUPProfile;
 
     COMMAND Orders AS PbillFunc;
     COMMAND _Orders() AS PbillFunc WITH Comment=air.PosComment, Tags=[Tag1, air.Tag2];
-    COMMAND Orders2(untill.orders) AS PbillFunc;
-    COMMAND Orders3(order untill.orders, untill.pbill) AS PbillFunc;
+    COMMAND Orders2(Untill.Orders) AS PbillFunc;
+    COMMAND Orders3(Order Untill.Orders, Untill.PBill) AS PbillFunc;
 
     QUERY Query1 RETURNS QueryResellerInfoResult AS PbillFunc;
-    QUERY _Query1() RETURNS air.QueryResellerInfoResult AS PbillFunc WITH Comment=air.PosComment, Tags=[Tag1, air.Tag2];
-    QUERY Query2(untill.orders) RETURNS QueryResellerInfoResult AS PbillFunc;
-    QUERY Query3(order untill.orders, untill.pbill) RETURNS QueryResellerInfoResult AS PbillFunc;
+    QUERY _Query1() RETURNS Air.QueryResellerInfoResult AS PbillFunc WITH Comment=Air.PosComment, Tags=[Tag1, Air.Tag2];
+    QUERY Query2(Untill.Orders) RETURNS QueryResellerInfoResult AS PbillFunc;
+    QUERY Query3(Order Untill.Orders, Untill.PBill) RETURNS QueryResellerInfoResult AS PbillFunc;
 
 
     GRANT ALL ON ALL TABLES WITH TAG untill.Backoffice TO LocationManager;
     GRANT INSERT,UPDATE ON ALL TABLES WITH TAG sys.ODoc TO LocationUser;
-    GRANT SELECT ON TABLE untill.orders TO LocationUser;
+    GRANT SELECT ON TABLE Untill.Orders TO LocationUser;
     GRANT EXECUTE ON COMMAND Orders TO LocationUser;
     GRANT EXECUTE ON QUERY TransactionHistory TO LocationUser;
     GRANT EXECUTE ON ALL QUERIES WITH TAG PosTag TO LocationUser;
 
-    TABLE ws_table OF CDOC, air.SomeType (
-        psname text,
-        TABLE child (
-            number int				
+    TABLE WsTable OF CDOC, Air.SomeType (
+        PsName text,
+        TABLE Child (
+            Number int				
         )
     );	
 
@@ -77,7 +77,7 @@ WORKSPACE ws (
         Kind int32, 
         Number int32, 
         XZReportWDocID id
-    ) AS RESULT OF air.UpdateXZReportsView
+    ) AS RESULT OF Air.UpdateXZReportsView
     WITH 
         PrimaryKey="(Year), Month, Day, Kind, Number",
         Comment=PosComment;
