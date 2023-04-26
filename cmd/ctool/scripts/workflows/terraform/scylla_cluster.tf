1ff4997bb222924da9ca162b9318583606c3bd59
AlekseiPonomarev <paa@sigma-soft.spb.ru>
2023-04-26 18:42:51 +0300

    update integration-test action

--- cmd/ctool/scripts/workflows/terraform/scylla_cluster.tf
+++ cmd/ctool/scripts/workflows/terraform/scylla_cluster.tf
@@ -1,6 +1,5 @@
 provider "aws" {
   region = "us-east-2"
-  shared_credentials_files = [""]
 }
 
 resource "aws_vpc" "scylla_cluster_vpc" {
@@ -26,52 +25,52 @@ resource "aws_subnet" "scylla_subnet" {
   }
 }
 
-resource "aws_network_interface" "node-00" {
+resource "aws_network_interface" "node_00" {
   subnet_id   = aws_subnet.scylla_subnet.id
   private_ips = ["10.0.0.11"]
   security_groups = [aws_security_group.scylla_hosts.id]
   tags = {
-    Name = "node-00-interface"
+    Name = "node_00_interface"
   }
 }
 
-resource "aws_network_interface" "node-01" {
+resource "aws_network_interface" "node_01" {
   subnet_id   = aws_subnet.scylla_subnet.id
   private_ips = ["10.0.0.12"]
   security_groups = [aws_security_group.scylla_hosts.id]
   tags = {
-    Name = "node-01-interface"
+    Name = "node_01_interface"
   }
 }
 
-resource "aws_network_interface" "node-02" {
+resource "aws_network_interface" "node_02" {
   subnet_id   = aws_subnet.scylla_subnet.id
   private_ips = ["10.0.0.13"]
   security_groups = [aws_security_group.scylla_hosts.id]
   tags = {
-    Name = "node-02-interface"
+    Name = "node_02_interface"
   }
 }
 
-resource "aws_network_interface" "node-03" {
+resource "aws_network_interface" "node_03" {
   subnet_id   = aws_subnet.scylla_subnet.id
   private_ips = ["10.0.0.14"]
   security_groups = [aws_security_group.scylla_hosts.id]
   tags = {
-    Name = "node-03-interface"
+    Name = "node_03_interface"
   }
 }
 
-resource "aws_network_interface" "node-04" {
+resource "aws_network_interface" "node_04" {
   subnet_id   = aws_subnet.scylla_subnet.id
   private_ips = ["10.0.0.15"]
   security_groups = [aws_security_group.scylla_hosts.id]
   tags = {
-    Name = "node-04-interface"
+    Name = "node_04_interface"
   }
 }
 
-resource "aws_instance" "node-00" {
+resource "aws_instance" "node_00" {
   ami = "ami-0568936c8d2b91c4e"
   instance_type = "i3.large"
   root_block_device {
@@ -80,14 +79,48 @@ resource "aws_instance" "node-00" {
   }
   placement_group = aws_placement_group.scylla_placement_group.id
   network_interface {
-    network_interface_id = aws_network_interface.node-00.id
+    network_interface_id = aws_network_interface.node_00.id
     device_index         = 0
   }
+
   key_name = "amazonKey"
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = "${var.ssh_private_key}"
+    host        = "${aws_instance.node_00.public_ip}"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+    "echo '${var.ssh_private_key}' > /tmp/amazonKey.pem",
+    "chmod 400 /tmp/amazonKey.pem",
+    "eval $(ssh-agent -s)",
+    "ssh-add /tmp/amazonKey.pem",
+    "mkdir -p ~/.ssh", 
+    "touch ~/.ssh/ssh_auth_sock",
+    "chmod 600 ~/.ssh/ssh_auth_sock",
+    "echo $SSH_AUTH_SOCK > ~/.ssh/ssh_auth_sock",
+    "rm -f /tmp/amazonKey.pem",
+    "git clone https://AlekseiPonomarev:'${var.gh_token}'@github.com/AlekseiPonomarev/testsdata",
+    "cd ~/testsdata/cmd/ctool/scripts/drafts",
+    "chmod +x *.sh",
+    "~/testsdata/cmd/ctool/scripts/drafts/ctool-scylla-deploy.sh 10.0.0.12 10.0.0.13 10.0.0.14"
+    ]
+  }
+
+  tags = {
+    Name = "node_00"
+  }
+
 }
 
+output "public_ip_node_00" {
+  value = aws_instance.node_00.public_ip
+}
 
-resource "aws_instance" "node-01" {
+resource "aws_instance" "node_01" {
   ami = "ami-0568936c8d2b91c4e"
   instance_type = "i3.large"
   root_block_device {
@@ -96,14 +129,18 @@ resource "aws_instance" "node-01" {
   }
   placement_group = aws_placement_group.scylla_placement_group.id
   network_interface {
-    network_interface_id = aws_network_interface.node-01.id
+    network_interface_id = aws_network_interface.node_01.id
     device_index         = 0
   }
   key_name = "amazonKey"
 }
 
+output "public_ip_node_01" {
+  value = aws_instance.node_01.public_ip
+}
 
-resource "aws_instance" "node-02" {
+
+resource "aws_instance" "node_02" {
   ami = "ami-0568936c8d2b91c4e"
   instance_type = "i3.large"
   root_block_device {
@@ -112,13 +149,18 @@ resource "aws_instance" "node-02" {
   }
   placement_group = aws_placement_group.scylla_placement_group.id
   network_interface {
-    network_interface_id = aws_network_interface.node-02.id
+    network_interface_id = aws_network_interface.node_02.id
     device_index         = 0
   }
   key_name = "amazonKey"
 }
 
-resource "aws_instance" "node-03" {
+output "public_ip_node_02" {
+  value = aws_instance.node_02.public_ip
+}
+
+
+resource "aws_instance" "node_03" {
   ami = "ami-0568936c8d2b91c4e"
   instance_type = "i3.large"
   root_block_device {
@@ -127,13 +169,17 @@ resource "aws_instance" "node-03" {
   }
   placement_group = aws_placement_group.scylla_placement_group.id
   network_interface {
-    network_interface_id = aws_network_interface.node-03.id
+    network_interface_id = aws_network_interface.node_03.id
     device_index         = 0
   }
   key_name = "amazonKey"
 }
 
-resource "aws_instance" "node-04" {
+output "public_ip_node_03" {
+  value = aws_instance.node_03.public_ip
+}
+
+resource "aws_instance" "node_04" {
   ami = "ami-0568936c8d2b91c4e"
   instance_type = "i3.large"
   root_block_device {
@@ -142,12 +188,15 @@ resource "aws_instance" "node-04" {
   }
   placement_group = aws_placement_group.scylla_placement_group.id
   network_interface {
-    network_interface_id = aws_network_interface.node-04.id
+    network_interface_id = aws_network_interface.node_04.id
     device_index         = 0
   }
   key_name = "amazonKey"
 }
 
+output "public_ip_node_04" {
+  value = aws_instance.node_04.public_ip
+}
 
 resource "aws_internet_gateway" "gw" { vpc_id = aws_vpc.scylla_cluster_vpc.id }
 
