75fffb19665cddf82a6629cf6a4c2d428d8bca2c
alisher <alisher.nurmanov@inbox.ru>
2023-04-28 18:05:18 +0300

    fix

--- cmd/edger/controllers_test.go
+++ cmd/edger/controllers_test.go
@@ -17,9 +17,11 @@ func Test_CommandController(t *testing.T) {
 	require.NoError(t, err)
 
 	tests := []struct {
-		name       string
-		sp         CommandSP
-		expectedPV *CommandPV
+		name             string
+		sp               CommandSP
+		expectedStdout   string
+		expectedStderr   string
+		expectedExitCode int
 	}{
 		{
 			name: `stdout,exitcode=0`,
@@ -27,13 +29,9 @@ func Test_CommandController(t *testing.T) {
 				Cmd:  "echo",
 				Args: []string{"hello", "world"},
 			},
-			expectedPV: &CommandPV{
-				Cmd:      "echo",
-				Args:     []string{"hello", "world"},
-				Stdout:   "hello world",
-				Stderr:   "",
-				ExitCode: 0,
-			},
+			expectedStdout:   "hello world",
+			expectedStderr:   "",
+			expectedExitCode: 0,
 		},
 		{
 			name: `stderr,exitcode=0`,
@@ -41,41 +39,19 @@ func Test_CommandController(t *testing.T) {
 				Cmd:  `sh`,
 				Args: []string{`-c`, `echo hello >&2`},
 			},
-			expectedPV: &CommandPV{
-				Cmd:      `sh`,
-				Args:     []string{`-c`, `echo hello >&2`},
-				Stdout:   "",
-				Stderr:   "hello",
-				ExitCode: 0,
-			},
+			expectedStdout:   "",
+			expectedStderr:   "hello",
+			expectedExitCode: 0,
 		},
 		{
 			name: `stderr,exitcode=1`,
 			sp: CommandSP{
-				Cmd:  "pwd",
-				Args: []string{`unused param`},
-			},
-			expectedPV: &CommandPV{
-				Cmd:      "pwd",
-				Args:     []string{`unused param`},
-				Stdout:   "",
-				Stderr:   "usage: pwd [-L | -P]",
-				ExitCode: 1,
-			},
-		},
-		{
-			name: `exitcode=1`,
-			sp: CommandSP{
 				Cmd:  "ls",
 				Args: []string{`/non/existent/directory`},
 			},
-			expectedPV: &CommandPV{
-				Cmd:      "ls",
-				Args:     []string{`/non/existent/directory`},
-				Stdout:   "",
-				Stderr:   "ls: /non/existent/directory: No such file or directory",
-				ExitCode: 1,
-			},
+			expectedStdout:   "",
+			expectedStderr:   "ls: /non/existent/directory: No such file or directory",
+			expectedExitCode: 1,
 		},
 		{
 			name: `exitcode=0`,
@@ -83,13 +59,19 @@ func Test_CommandController(t *testing.T) {
 				Cmd:  "pwd",
 				Args: []string{},
 			},
-			expectedPV: &CommandPV{
-				Cmd:      "pwd",
-				Args:     []string{},
-				Stdout:   wd,
-				Stderr:   "",
-				ExitCode: 0,
+			expectedStdout:   wd,
+			expectedStderr:   "",
+			expectedExitCode: 0,
+		},
+		{
+			name: `exitcode=1`,
+			sp: CommandSP{
+				Cmd:  "ls",
+				Args: []string{`/non/existent/directory`, `>/dev/null 2>&1`},
 			},
+			expectedStdout:   "",
+			expectedStderr:   "ls: /non/existent/directory: No such file or directory",
+			expectedExitCode: 1,
 		},
 	}
 
@@ -97,7 +79,9 @@ func Test_CommandController(t *testing.T) {
 		t.Run(test.name, func(t *testing.T) {
 			_, pv, _ := CommandController(``, test.sp, struct{}{})
 
-			require.Equal(t, *test.expectedPV, *pv)
+			require.Equal(t, len(test.expectedStdout) > 0, len(pv.Stdout) > 0)
+			require.Equal(t, len(test.expectedStderr) > 0, len(pv.Stderr) > 0)
+			require.Equal(t, test.expectedExitCode, pv.ExitCode)
 		})
 	}
 }
