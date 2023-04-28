1fc5eb33bc34f1050c6ddb420c50911975d2799a
Denis Gribanov <instance6@gmail.com>
2023-04-28 18:19:54 +0300

    go on

--- pkg/utils/appwsid.go
+++ pkg/utils/appwsid.go
@@ -10,6 +10,6 @@ import "github.com/voedger/voedger/pkg/istructs"
 func GetAppWSID(wsid istructs.WSID, appWSAmount istructs.AppWSAmount) istructs.WSID {
 	baseWSID := wsid.BaseWSID()
 	appWSNumber := baseWSID % istructs.WSID(appWSAmount)
-	appWSID := istructs.FirstBaseAppWSID + appWSNumber
-	return istructs.NewWSID(wsid.ClusterID(), appWSID)
+	baseAppWSID := istructs.FirstBaseAppWSID + appWSNumber
+	return istructs.NewWSID(istructs.MainClusterID, baseAppWSID)
 }
