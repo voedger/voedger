77569bea00b28485ba9121789363d7ca65036267
alisher <alisher.nurmanov@inbox.ru>
2023-05-16 19:28:09 +0300

    added stdin sp reader via edger's run command

---
+++ cmd/edger/consts.go
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2023-present unTill Pro, Ltd.
+ * @author: Alisher Nurmanov
+ */
+
+package main
+
+const (
+	SPTypeCommand = "command"
+	SPTypeDocker  = "docker"
+)
+
+const (
+	NumCommandControllerRoutines = 5
+	NumDockerControllerRoutines  = 5
+)
---
+++ cmd/edger/reporters.go
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2023-present unTill Pro, Ltd.
+ * @author: Alisher Nurmanov
+ */
+
+package main
+
+func CommandReporter(_ string, _ *CommandPV) (err error) {
+	return nil
+}
+
+func DockerReporter(_ string, _ *DockerPV) (err error) {
+	return nil
+}
