21fb4cfd9dae7f8e0fdd822883ffe8774f924591
Denis Gribanov <instance6@gmail.com>
2023-05-19 10:42:31 +0300

    go on

--- pkg/utils/appwsid_test.go
+++ pkg/utils/appwsid_test.go
@@ -19,13 +19,13 @@ func TestBasicUsage_GetAppWSID(t *testing.T) {
 		appWSAmount     istructs.AppWSAmount
 		expectedAppWSID istructs.WSID
 	}{
-		{1, 1, istructs.MaxPseudoBaseWSID + 1},
-		{2, 1, istructs.MaxPseudoBaseWSID + 1},
-		{3, 1, istructs.MaxPseudoBaseWSID + 1},
-		{1, 10, istructs.MaxPseudoBaseWSID + 2},
-		{8, 10, istructs.MaxPseudoBaseWSID + 9},
-		{10, 10, istructs.MaxPseudoBaseWSID + 1},
-		{11, 10, istructs.MaxPseudoBaseWSID + 2},
+		{1, 1, istructs.NewWSID(istructs.MainClusterID, istructs.MaxPseudoBaseWSID+1)},
+		{2, 1, istructs.NewWSID(istructs.MainClusterID, istructs.MaxPseudoBaseWSID+1)},
+		{3, 1, istructs.NewWSID(istructs.MainClusterID, istructs.MaxPseudoBaseWSID+1)},
+		{1, 10, istructs.NewWSID(istructs.MainClusterID, istructs.MaxPseudoBaseWSID+2)},
+		{8, 10, istructs.NewWSID(istructs.MainClusterID, istructs.MaxPseudoBaseWSID+9)},
+		{10, 10, istructs.NewWSID(istructs.MainClusterID, istructs.MaxPseudoBaseWSID+1)},
+		{11, 10, istructs.NewWSID(istructs.MainClusterID, istructs.MaxPseudoBaseWSID+2)},
 	}
 
 	for _, c := range cases {
