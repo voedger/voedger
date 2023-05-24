2dd3d1e3279aba4affa30c554d9db6bb52e805c3
Alisher <38488987+anurmanov@users.noreply.github.com>
2023-05-24 17:57:27 +0600

    94 edger read control messages from stdin (#165)
    * Commit for keeping notes in branch
    
    * Commit for keeping notes in branch
    
    * Commit for keeping notes in branch
    
    * added stdin sp reader via edger's run command
    
    * 124 appdef definitions hierarchy (#146)
    
    * Commit for keeping notes in branch
    
    * Commit for keeping notes in branch
    
    * added stdin sp reader via edger's run command
    
    * Commit for keeping notes in branch
    
    * fix
    
    ---------
    
    Co-authored-by: Nikolay <36446176+mail2nnv@users.noreply.github.com>

---
+++ cmd/edger/run.go
@@ -0,0 +1,102 @@
+/*
+ * Copyright (c) 2023-present unTill Pro, Ltd.
+ * @author: Alisher Nurmanov
+ */
+
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"time"
+
+	"github.com/spf13/cobra"
+	"github.com/untillpro/goutils/logger"
+
+	"github.com/voedger/voedger/pkg/ctrlloop"
+)
+
+type InputControlMessage struct {
+	Type  string
+	Value json.RawMessage
+}
+
+var inputStreamReadingInterval time.Duration = 0
+
+func newRunEdgerCmd() *cobra.Command {
+	cmd := cobra.Command{
+		Use:   "run",
+		Short: "Runs edger and processes SP values from stdin",
+		Run: func(c *cobra.Command, args []string) {
+			commandInCh := make(chan ctrlloop.ControlMessage[string, CommandSP])
+			commandCtrlloopWaitFunc := ctrlloop.New(CommandController, CommandReporter, NumCommandControllerRoutines, commandInCh, time.Now)
+			defer commandCtrlloopWaitFunc()
+			defer close(commandInCh)
+
+			dockerInCh := make(chan ctrlloop.ControlMessage[string, DockerSP])
+			dockerCtrlloopWaitFunc := ctrlloop.New(DockerController, DockerReporter, NumDockerControllerRoutines, dockerInCh, time.Now)
+			defer dockerCtrlloopWaitFunc()
+			defer close(dockerInCh)
+
+			runEdger(c.Context(), os.Stdin, commandInCh, dockerInCh)
+		},
+	}
+
+	return &cmd
+}
+
+func runEdger(ctx context.Context, r io.Reader, commandInCh chan ctrlloop.ControlMessage[string, CommandSP], dockerInCh chan ctrlloop.ControlMessage[string, DockerSP]) {
+	decoder := json.NewDecoder(r)
+
+	var decodingErr error
+	for decodingErr != io.EOF && ctx.Err() == nil {
+		var input InputControlMessage
+		decodingErr = decoder.Decode(&input)
+		if decodingErr != nil {
+			if decodingErr != io.EOF {
+				logger.Verbose("error decoding JSON:", decodingErr)
+			}
+			continue
+		}
+
+		switch input.Type {
+		case SPTypeCommand:
+			m := getControlMessage[string, CommandSP](input)
+			if m != nil {
+				commandInCh <- *m
+			}
+		case SPTypeDocker:
+			m := getControlMessage[string, DockerSP](input)
+			if m != nil {
+				dockerInCh <- *m
+			}
+		default:
+			logger.Verbose(fmt.Sprintf("unknown sp type: %s", input.Type))
+		}
+		time.Sleep(inputStreamReadingInterval)
+	}
+}
+
+func decodeControlMessage[Key comparable, SP any](input InputControlMessage) *ctrlloop.ControlMessage[Key, SP] {
+	var sp ctrlloop.ControlMessage[Key, SP]
+	if err := json.Unmarshal(input.Value, &sp); err != nil {
+		logger.Verbose("error decoding")
+		return nil
+	}
+	return &sp
+}
+
+func getControlMessage[Key comparable, SP any](input InputControlMessage) *ctrlloop.ControlMessage[Key, SP] {
+	switch input.Type {
+	case SPTypeCommand:
+		return decodeControlMessage[Key, SP](input)
+	case SPTypeDocker:
+		return decodeControlMessage[Key, SP](input)
+	default:
+		logger.Verbose(fmt.Sprintf("unknown sp type: %s", input.Type))
+		return nil
+	}
+}
