f9fd3cdc627066c8d5004e2b85635fc75f6f6416
AlekseiPonomarev <paa@sigma-soft.spb.ru>
2023-05-24 16:29:22 +0300

    misc

--- cmd/ctool/scripts/drafts/mon-node-prepare.sh
+++ cmd/ctool/scripts/drafts/mon-node-prepare.sh
@@ -31,6 +31,24 @@ while [ $# -gt 0 ] && [ $count -lt 2 ]; do
   echo "Processing: $1"
   ssh $SSH_OPTIONS $SSH_USER@$1 "sudo mkdir -p /prometheus && mkdir -p ~/prometheus"
   ssh $SSH_OPTIONS $SSH_USER@$1 "sudo mkdir -p /alertmanager && mkdir -p ~/alertmanager"
+  ssh $SSH_OPTIONS $SSH_USER@$1 "sudo mkdir -p ~/grafana/provisioning/dashboards"
+  ssh $SSH_OPTIONS $SSH_USER@$1 "sudo mkdir -p ~/grafana/provisioning/datasources"
+
+   cat ./grafana/provisioning/dashboards/swarmprom-nodes-dash.json | \
+      ssh $SSH_OPTIONS $SSH_USER@$1 'cat > ~/grafana/provisioning/dashboards/swarmprom-nodes-dash.json'
+   cat ./grafana/provisioning/dashboards/swarmprom-prometheus-dash.json | \
+      ssh $SSH_OPTIONS $SSH_USER@$1 'cat > ~/grafana/provisioning/dashboards/swarmprom-prometheus-dash.json'
+   cat ./grafana/provisioning/dashboards/swarmprom-services-dash.json | \
+      ssh $SSH_OPTIONS $SSH_USER@$1 'cat > ~/grafana/provisioning/dashboards/swarmprom-services-dash.json'
+   cat ./grafana/provisioning/dashboards/swarmprom_dashboards.yml | \
+      ssh $SSH_OPTIONS $SSH_USER@$1 'cat > ~/grafana/provisioning/dashboards/swarmprom_dashboards.yml'
+
+  cat ./grafana/provisioning/datasources/datasource.yml | \
+      sed "s/{se}/$1/g" \
+      | ssh $SSH_OPTIONS $SSH_USER@$1 'cat > ~/grafana/provisioning/datasources/datasource.yml'
+
+  cat ./grafana/grafana.ini | ssh $SSH_OPTIONS $SSH_USER@$1 'cat > ~/grafana/grafana.ini'
+
 
   cat ./prometheus/prometheus.yml | \
       sed "s/{scylla1}/$scylla1/g; s/{scylla2}/$scylla2/g; s/{scylla3}/$scylla3/g; s/{se1}/$se1/g; s/{se2}/$se2/g; s/{mon_label}/mon$((count+1))/g" \
@@ -41,6 +59,7 @@ while [ $# -gt 0 ] && [ $count -lt 2 ]; do
 
   ssh $SSH_OPTIONS $SSH_USER@$1 "sudo chown -R 65534:65534 /prometheus"
   ssh $SSH_OPTIONS $SSH_USER@$1 "sudo chown -R 65534:65534 /alertmanager"
+  ssh $SSH_OPTIONS $SSH_USER@$1 "sudo chown -R 472:472 /var/lib/grafana"
 
   count=$((count+1))
 
