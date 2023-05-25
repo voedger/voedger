c649e92eea4ef8413df4167047642afef082aeb9
Michael <m.saigachenko@dev.untill.com>
2023-05-25 19:42:45 +0200

    pkg/parser: linters

--- pkg/parser/const.go
+++ pkg/parser/const.go
@@ -26,4 +26,4 @@ const (
 	sysBytes   = "blob"
 )
 
-const maxNestedTableContainerOccurences = 100 // FIXME: 100 container occurances
+const maxNestedTableContainerOccurences = 100 // FIXME: 100 container occurences
