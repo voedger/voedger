b67b7dd31a3dc234b4c3d94e6d62526dd62ab060
AlekseiPonomarev <paa@sigma-soft.spb.ru>
2023-06-01 12:45:13 +0300

    misc

--- cmd/ctool/scripts/drafts/db-cluster-start.sh
+++ cmd/ctool/scripts/drafts/db-cluster-start.sh
@@ -10,25 +10,25 @@ set -euo pipefail
 set -x
 
 if [[ $# -ne 3 ]]; then
-  echo "Usage: $0 <scylla1> <scylla2> <scylla3>"
+  echo "Usage: $0 <DBNode1> <DBNode2> <DBNode3>"
   exit 1
 fi
 
 SSH_USER=$LOGNAME
 SSH_OPTIONS='-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o LogLevel=ERROR'
 
-scylla1=$1
-scylla2=$2
-scylla3=$3
+DBNode1=$1
+DBNode2=$2
+DBNode3=$3
 
 # Replace the template values in the YAML file with the arguments (scylla nodes ip addresses)
 # and store as prod compose file for start swarm services
 cat docker-compose-template.yml | \
-    sed "s/{scylla1}/$scylla1/g; s/{scylla2}/$scylla2/g; s/{scylla3}/$scylla3/g" \
+    sed "s/{{.DBNode1})/$DBNode1/g; s/{{.DBNode2}}/$DBNode2/g; s/{{.DBNode3}}/$DBNode3/g" \
     > ./docker-compose.yml
 
 cat ./docker-compose.yml | ssh $SSH_OPTIONS $SSH_USER@$1 'cat > ~/docker-compose.yml'
 
-ssh $SSH_OPTIONS $SSH_USER@$1 "docker stack deploy --compose-file ~/docker-compose.yml DBMSDockerStack"
+ssh $SSH_OPTIONS $SSH_USER@$1 "docker stack deploy --compose-file ~/docker-compose.yml DBDockerStack"
 
 set +x
