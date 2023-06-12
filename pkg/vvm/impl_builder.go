4e41cbcfbb1361c0c37ff2f5fc4c9b4287d787cd
Denis Gribanov <instance6@gmail.com>
2023-06-12 16:57:06 +0300

    misc

--- pkg/vvm/impl_builder.go
+++ pkg/vvm/impl_builder.go
@@ -12,6 +12,7 @@ import (
 	"github.com/voedger/voedger/pkg/extensionpoints"
 	"github.com/voedger/voedger/pkg/istructs"
 	istructsmem "github.com/voedger/voedger/pkg/istructsmem"
+	"github.com/voedger/voedger/pkg/parser"
 )
 
 func (hap VVMAppsBuilder) Add(appQName istructs.AppQName, builder apps.AppBuilder) {
@@ -28,49 +29,74 @@ func (hap VVMAppsBuilder) PrepareAppsExtensionPoints() map[istructs.AppQName]ext
 	return seps
 }
 
+func buildSchemasASTs(adf appdef.IAppDefBuilder, ep extensionpoints.IExtensionPoint) {
+	epPackageSchemasASTs := ep.ExtensionPoint(apps.EPPackageSchemasASTs)
+	packageSchemaASTs := []*parser.PackageSchemaAST{}
+	epPackageSchemasASTs.Iterate(func(eKey extensionpoints.EKey, value interface{}) {
+		packageName := eKey.(string)
+		fileSchemaASTs := value.([]*parser.FileSchemaAST)
+		packageSchemaAST, err := parser.MergeFileSchemaASTs(packageName, fileSchemaASTs)
+		if err != nil {
+			panic(err)
+		}
+		packageSchemaASTs = append(packageSchemaASTs, packageSchemaAST)
+	})
+	packageSchemas, err := parser.MergePackageSchemas(packageSchemaASTs)
+	if err != nil {
+		panic(err)
+	}
+	if err := parser.BuildAppDefs(packageSchemas, adf); err != nil {
+		panic(err)
+	}
+}
+
+func buildPostDocs(adf appdef.IAppDefBuilder, ep extensionpoints.IExtensionPoint) {
+	epPostDocs := ep.ExtensionPoint(apps.EPPostDocs)
+	epPostDocs.Iterate(func(eKey extensionpoints.EKey, value interface{}) {
+		epPostDoc := value.(extensionpoints.IExtensionPoint)
+		postDocQName := eKey.(appdef.QName)
+		postDocDesc := epPostDoc.Value().(PostDocDesc)
+
+		var doc appdef.IFieldsBuilder
+		switch postDocDesc.Kind {
+		case appdef.DefKind_GDoc:
+			doc = adf.AddGDoc(postDocQName)
+		case appdef.DefKind_CDoc:
+			if postDocDesc.IsSingleton {
+				doc = adf.AddSingleton(postDocQName)
+			} else {
+				doc = adf.AddCDoc(postDocQName)
+			}
+		case appdef.DefKind_WDoc:
+			doc = adf.AddWDoc(postDocQName)
+		case appdef.DefKind_ODoc:
+			doc = adf.AddODoc(postDocQName)
+		default:
+			panic(fmt.Errorf("document «%s» has unexpected definition kind «%v»", postDocQName, postDocDesc.Kind))
+		}
+
+		epPostDoc.Iterate(func(eKey extensionpoints.EKey, value interface{}) {
+			postDocField := value.(PostDocFieldType)
+			if len(postDocField.VerificationKinds) > 0 {
+				doc.AddVerifiedField(eKey.(string), postDocField.Kind, postDocField.Required, postDocField.VerificationKinds...)
+			} else {
+				doc.AddField(eKey.(string), postDocField.Kind, postDocField.Required)
+			}
+		})
+	})
+}
+
 func (hap VVMAppsBuilder) Build(cfgs istructsmem.AppConfigsType, apis apps.APIs, appsEPs map[istructs.AppQName]extensionpoints.IExtensionPoint) (vvmApps VVMApps) {
-	for appQName, builders := range hap {
+	for appQName, appBuilders := range hap {
 		adf := appdef.New()
 		appEPs := appsEPs[appQName]
 		cfg := cfgs.AddConfig(appQName, adf)
-		for _, builder := range builders {
+		for _, builder := range appBuilders {
 			builder(apis, cfg, adf, appEPs)
 		}
-		epPostDocs := appEPs.ExtensionPoint(apps.EPPostDocs)
-		epPostDocs.Iterate(func(eKey extensionpoints.EKey, value interface{}) {
-			epPostDoc := value.(extensionpoints.IExtensionPoint)
-			postDocQName := eKey.(appdef.QName)
-			postDocDesc := epPostDoc.Value().(PostDocDesc)
-
-			var doc appdef.IFieldsBuilder
-			switch postDocDesc.Kind {
-			case appdef.DefKind_GDoc:
-				doc = adf.AddGDoc(postDocQName)
-			case appdef.DefKind_CDoc:
-				if postDocDesc.IsSingleton {
-					doc = adf.AddSingleton(postDocQName)
-				} else {
-					doc = adf.AddCDoc(postDocQName)
-				}
-			case appdef.DefKind_WDoc:
-				doc = adf.AddWDoc(postDocQName)
-			case appdef.DefKind_ODoc:
-				doc = adf.AddODoc(postDocQName)
-			default:
-				panic(fmt.Errorf("document «%s» has unexpected definition kind «%v»", postDocQName, postDocDesc.Kind))
-			}
-
-			epPostDoc.Iterate(func(eKey extensionpoints.EKey, value interface{}) {
-				postDocField := value.(PostDocFieldType)
-				if len(postDocField.VerificationKinds) > 0 {
-					doc.AddVerifiedField(eKey.(string), postDocField.Kind, postDocField.Required, postDocField.VerificationKinds...)
-				} else {
-					doc.AddField(eKey.(string), postDocField.Kind, postDocField.Required)
-				}
-			})
-		})
+		buildSchemasASTs(adf, appEPs)
+		buildPostDocs(adf, appEPs)
 		vvmApps = append(vvmApps, appQName)
-		// TODO: remove it after https://github.com/voedger/voedger/issues/56
 		if _, err := adf.Build(); err != nil {
 			panic(err)
 		}
