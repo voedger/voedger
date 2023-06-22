1c20714dd4873f93e934bec1e26ef0a3bc25391d
Denis Gribanov <instance6@gmail.com>
2023-06-22 18:47:22 +0300

    go on

--- pkg/sys/invite/impl_subject.go
+++ pkg/sys/invite/impl_subject.go
@@ -58,9 +58,14 @@ func viewSubjectsIdxProjector(event istructs.IPLogEvent, st istructs.IState, int
 		login := cdocSubject.AsString(Field_Login)
 		skbViewSubjectsIdx.PutInt64(Field_LoginHash, coreutils.HashBytes([]byte(login)))
 		skbViewSubjectsIdx.PutString(Field_Login, login)
-		if err := st.MustNotExist(skbViewSubjectsIdx); err != nil {
+		_, ok , err := st.CanExist(skbViewSubjectsIdx)
+		if err != nil {
 			// notest
 			return err
+		}
+		if ok {
+			// already handled by the projector in async mode
+			return nil
 		}
 		subjectsIdxBuilder, err := intents.NewValue(skbViewSubjectsIdx)
 		if err != nil {
