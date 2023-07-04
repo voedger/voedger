414b40a12e2e58ab525c554f3f1a72ebde10674d
Nikolay <36446176+mail2nnv@users.noreply.github.com>
2023-07-04 15:57:00 +0300

    358 💊 fix istructsmem-internal-bytespool test (#360)
    * more accuracy test
    
    * more accuracy pool test

--- pkg/istructsmem/internal/bytespool/pool_test.go
+++ pkg/istructsmem/internal/bytespool/pool_test.go
@@ -15,12 +15,18 @@ func TestBasicUsage(t *testing.T) {
 
 	require := require.New(t)
 
+	var capacity int
+
 	t.Run("use", func(t *testing.T) {
 		b := Get()
 
 		require.Empty(b)
+		require.Zero(cap(b))
 
 		b = append(b, []byte{1, 2, 3}...)
+
+		capacity = cap(b)
+		require.Positive(capacity)
 
 		Put(b)
 	})
@@ -30,8 +36,11 @@ func TestBasicUsage(t *testing.T) {
 
 		require.Empty(b)
 
-		b = b[:3]
-		require.Equal([]byte{1, 2, 3}, b)
+		// there is no guarantee that the garbage collector will not clean up some element of the pool in parallel with your code
+		// see [issue 358](https://github.com/voedger/voedger/issues/358)
+		if c := cap(b); c > 0 {
+			require.Equal(c, capacity)
+		}
 
 		Put(b)
 	})
