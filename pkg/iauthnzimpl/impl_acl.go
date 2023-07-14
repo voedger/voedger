553b698bdff90b65c0722a2f1d72368080701398
Denis Gribanov <instance6@gmail.com>
2023-07-14 14:39:01 +0300

    misc

--- pkg/iauthnzimpl/impl_acl.go
+++ pkg/iauthnzimpl/impl_acl.go
@@ -294,4 +294,18 @@ var defaultACL = ACL{
 		},
 		policy: ACPolicy_Allow,
 	},
+	{
+		// https://github.com/voedger/voedger/issues/422
+		desc: "grant exec on few funcs to role air.UntillPaymentsReseller",
+		pattern: PatternType{
+			qNamesPattern: []appdef.QName{
+				qNameQryGetDailyPayoutCfg,
+				qNameCmdUpdateScheduledPayout,
+				qNameQryGetUPTransfers,
+				qNameCmdCreateUPTransfer,
+			},
+			principalsPattern: [][]iauthnz.Principal{{{Kind: iauthnz.PrincipalKind_Role, QName: qNameRoleUntillPaymentsReseller}}},
+		},
+		policy: ACPolicy_Allow,
+	},
 }
