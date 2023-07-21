f3026a199daed2a308b05a5a2b8068cf66b874ae
ivvist <ivvist@gmail.com>
2023-07-21 13:43:11 +0200

    comments above fields

--- pkg/parser/sql_example_app/vrestaurant/package.sql
+++ pkg/parser/sql_example_app/vrestaurant/package.sql
@@ -1,14 +1,13 @@
 SCHEMA vrestaurant;
 
 WORKSPACE Restaurant (
-    DESCRITOR (
+    DESCRIPTOR (
 	    Address text,
 	    Currency text,
 	    Phone text,
 	    OpenTimeStamp int64, -- TODO timestamp
 	    OwnerName text
     );
-
 
     ROLE LocationUser;
     ROLE LocationManager;
@@ -20,190 +19,188 @@ WORKSPACE Restaurant (
     -- CDOC data schemes
 
     -- TABLE BOEntity : is an Abstract base data struct for many CDOC tables
-    --   Name   : Entity name 
-    --   Number : Entity number
-    TABLE BOEntity INHERITS CDoc( -- TODO ABSTRACT
-        Name text,
+    TABLE BOEntity INHERITS CDoc( 
+        Name text, 
         Number int
     ) WITH Tags=(BackofficeTag);
 
     -- TABLE Person : is an Abstract data struct for Waiters, Clients, Adminitsrators, Manager
-    --   Address    : Person address
-    --   Email      : email of person
-    --   Phone      : phone number of person
-    --   Picture    : Image of person
-    TABLE Person INHERITS BOEntity ( -- ABSTRACT
+    TABLE Person INHERITS BOEntity ( 
 	    Address text,
         Email text,
 	    Phone text,
         Picture blob
     ) WITH Tags=(BackofficeTag);
 
-    -- TABLE Clients    : describes restaurant client entity
-    --   Datebirth      : Date of birth, can be needed to define age limitations
-    --   Card           : Client payment card number, used for payments in Restaurant
-    --   Discount_percent : Percent of permanent discount
+    -- TABLE Client   : describes restaurant client entity
     TABLE Client INHERITS Person(
-        Datebirth int64, -- access to alcohol
-        Card text,
-        DiscountPercent int
-    ) WITH Comment="Client defines customer data";
+        -- access to alcohol
+        Datebirth int64, 
+        -- payment card number, used for payments in Restaurant
+        Card text,       
+        -- percent of permanent discount
+        DiscountPercent int 
+    );
 
-    -- TABLE POSUsers   : describes restaurant user entity (Waiter/Administrator/Manager)
-    --   Code       : personal code in inner login syustem    
-    --   Position   : Name of position in Restaurant
-    --   Wage       : Wage/salary rate
+    -- TABLE Register   : describes payment resgitration devices
+    TABLE Register INHERITS Person(
+        Code text -- personal code in inner login system    
+    );
+
+    -- TABLE Position   : Restaurant job list
+    TABLE Position INHERITS BOEntity(
+    );
+
+    -- TABLE POSUser   : describes restaurant user entity (Waiter/Administrator/Manager)
     TABLE POSUser INHERITS Person(
-        Code text,
-	Position text, -- PositionID ref(Position)
+        -- personal code in inner login system    
+        Code text, 
+	    PositionID ref(Position),
+        -- wage/salary rate
         Wage float32
-    ) WITH Comment="Waiter defines restaurant staff data";
+    );
 
     -- TABLE TablePlan    : describes Physical plan of tables/bar counters. etc. in Restaurant.
-    --   Picture          : Image of restaurant plan
-    -- TABLE TableItems   : Each table plan has several tables
-    --   Tableno          : Number of table    
-    --   Color            : Default table color on plan 
-    --   Chairs           : Number of chiar, assigned to the table
-    --   Left, Top        : Table position on Table plan
-    --   Width, Height    : Table size
     TABLE TablePlan INHERITS BOEntity (
-        Picture blob,
+        -- Image of restaurant plan
+        Picture blob, 
+        Width int,
+        Height int,
         TableItem TABLE TableItem (
-            Tableno int,
-    	    Color int,
-            Chairs int,
+            Tableno int,  
+            -- color of empty table
+    	    Color int,    
+            -- number of table chairs
+            Chairs int,   
             Left int,
             Top int,
- 	    Width int,
-	    Height int
+ 	        Width int,
+	        Height int
         )
     ) WITH Comment="Table plan defines restaurant tables schema";
 
     -- TABLE departments : defines Restaurant department button entity
-    TABLE Departments INHERITS BOEntity (
-    ) WITH Comment="Department defines department for article sets";
+    TABLE Department INHERITS BOEntity (
+    );
 
-    -- TABLE Articles : defines Restaurant articles button entity
-    --   Barcode      : article barcode to order by scanner
-    --   Price        : article price
-    --   VatPercent   : Vate in percent
-    --   Vat          : Absolut Vate value
-    TABLE Articles INHERITS BOEntity(
-        DepartamentID ref(Departments),
-        Barcode text,
-        Price float32,
-        VatPercent float32,
-        Vat float32
-    ) WITH Comment="Articles defines restaurant article sets";
+    -- TABLE Article : defines Restaurant articles button entity
+    TABLE Article INHERITS BOEntity(
+        DepartamentID ref(Department),
+        -- article barcode to order by scanner
+        Barcode text,  
+        -- article sale price 
+        Price float32, 
+        -- V.A.T. in percent
+        VatPercent float32, 
+        -- Absolut V.A.T. value 
+        Vat float32         
+    );
 
-    -- TABLE payment_types : defines types of payment modes, using in Restaurant
-    --   Kind               : Inner value of type
-    TABLE PaymentTypes INHERITS BOEntity(
-        Kind int
-    ) WITH Comment="Payment type defines parameters of Payment method";
+    -- TABLE PaymentType : defines types of payment modes, using in Restaurant
+    TABLE PaymentType INHERITS BOEntity(
+        --inner value of type
+        Kind int 
+    );
 
-    -- TABLE Transactions   : defines parameters of table, occupied by client
-    --   Tableno            : Number of table
-    --   Name               : Name of transaction
-    --   OpenTimeStamp      : Time of very first order on table
-    --   CloseTimeStamp     : Time of final payment and closing table transaction
-    --   UserID            : Ref on User, who made very first order on table
-    --   Client          : Ref on Client, paying the bill
-    TABLE Transactions INHERITS WDoc(
+    -- TABLE Transaction   : defines parameters of table, occupied by client
+    TABLE Transaction INHERITS WDoc(
         Name text,
         Number int,
         Tableno int, 
-        OpenTimeStamp int64,
+        --time of very first order on table
+        OpenTimeStamp int64, 
+        -- time of final payment and closing table transaction
         CloseTimeStamp int64,
-        UserID ref(POSUsers) NOT NULL, 
-        Client ref(Clients) NOT NULL 
-    ) WITH Tags=(PosTag), Comment="Transaction defines properties of ordered table";
+        -- POS user, who created made very first order
+        CreatorID ref(POSUser) NOT NULL, 
+        -- client, assigned to transaction
+        Client ref(Client) NOT NULL 
+    ) WITH Tags=(PosTag);
 
     -- TABLE Orders     : defines parameters of order on table. One transaction can have several orders
-    --   TransactionID : ref on parent transactions
-    --   OrdTimeStamp   : Time of creating order
-    --   UserID        : ref on user, who created the order
     -- TABLE order_items : the list articles, options, comments, from which order consists of
-    --   ArticleID     : ref on article(can be null)
-    --   Quantity       : Number of articles in order
-    --   Comment        : Text message, added to the order
-    --   Price          : Price of article, when order is created
-    --   VatPercent    : VAT% of article
-    --   Vat            : absolute value VAT of article
     TABLE Order INHERITS ODoc(
-        TransactionID ref(Transactions) NOT NULL, 
-        OrdTimeStamp int64,
-        UserID ref(POSUsers) NOT NULL, 
-        OrderItems TABLE OrderItem (
-            Order ref(Orders) NOT NULL,
-            ArticleID ref(Articles), -- can be null for text comments
-            Quantity int,
-            Comment text,
-            Price float32,
+        TransactionID ref(Transaction) NOT NULL, 
+        -- time of creating order
+        OrdTimeStamp int64, 
+        UserID ref(POSUser) NOT NULL, 
+        OrderItem TABLE OrderItem (
+            Order ref(Order) NOT NULL,
+            -- can be null for text comments
+            ArticleID ref(Article), 
+            -- number of articles in order
+            Quantity int,           
+            -- text message, added to the order
+            Comment text,           
+            Price int64,
             VatPercent float32,
             Vat float32
         )
-    ) WITH Tags=(PosTag), Comment="Order defines act of ordering";
+    ) WITH Tags=(PosTag);
 
-    -- TABLE Bill       : defines parameters of bill on table. One transaction can have several bills
-    --   UserID         : ref of User, who took created bill and takes payment -- TODO must be a field comment
-    --   Number         : Bill number, unique per cash register 
-    --   PayTimeStamp   : Time of Bill creating
+    -- TABLE Bill          : defines parameters of bill on table. One transaction can have several bills
     -- TABLE BillPayments  : Defines set of payment methods related to bill
-    --   PaymentType    : ref on payment_kind, defines kind of payment
-    --   Amount         : Amount of payment
     TABLE Bills INHERITS ODoc(
         TransactionID ref(Transaction) NOT NULL, 
+        --   ref of POSUser, who took created Transaction
        	AuthorID ref(POSUser) NOT NULL,
-	RegisterID ref(Register) NOT NULL,
-        -- Unique per register
-	Number int,
-	    
+	    RegisterID ref(Register) NOT NULL,
+        -- bill number, unique per cash register   
+	    Number int,  
+        -- time of Bill creating
         PayTimeStamp int64,
         Tips float32,
         BillPayments TABLE BillPayments (
-            Bill ref(Bills) NOT NULL,
+            Bill ref(Bill) NOT NULL,
             PaymentType ref(PaymentTypes) NOT NULL,
-            Amount float32
+            -- amount of payment
+            Amount int64
         )
-    ) WITH Tags=(PosTag), Comment="Bill defines act of payment";
+    ) WITH Tags=(PosTag);
 
     EXTENSION ENGINE BUILTIN (
-	    COMMAND MakeOrder(vrestaurant.Order)
-	    COMMAND MakePayment(vrestaurant.Payment)
+	    COMMAND MakeOrder(vrestaurant.Order);
+	    COMMAND MakePayment(vrestaurant.Payment);
 	
 	    PROJECTOR UpdateTableStatus
 	        ON COMMAND IN (Order, MakePayment)
 		INTENTS(View TableStatus);
+
+	    PROJECTOR UpdateSalesReport
+	        ON COMMAND IN (MakePayment)
+		INTENTS(View SalesPerDay);
+
     );
 
 -- ACLs
     GRANT ALL ON ALL TABLES WITH TAG BackofficeTag TO LocationManager;
     GRANT INSERT,UPDATE ON ALL TABLES WITH TAG sys.ODoc TO LocationUser;
-    GRANT SELECT ON TABLE Orders TO LocationUser;
-    GRANT EXECUTE ON COMMAND Orders TO LocationUser;
-    GRANT EXECUTE ON QUERY TransactionHistory TO LocationUser;
+    GRANT SELECT ON TABLE Order TO LocationUser;
+    GRANT EXECUTE ON COMMAND MakeOrder TO LocationUser;
+    GRANT EXECUTE ON COMMAND MakePayment TO LocationUser;
     GRANT EXECUTE ON ALL QUERIES WITH TAG PosTag TO LocationUser;
 
     -- VIEW TableStatus     : keeps actual status of table(free/occupied)
-    --  TableNumber             : Table number
-    --  Status   - state of table(free/occupied)
     VIEW TableStatus (
         TableNumber int,
+        --  status of table(free/occupied)
         Status int,
         PRIMARY KEY (TableNumber)
     ) AS RESULT OF UpdateTableStatus;
 
+    -- VIEW SalesPerDay     : sales report per day
     VIEW SalesPerDay(
         Year int32,
         Month int32,
         Day int32,
-
-	-- ???
-        Kind int32, 
         Number int32, 
-        XZReportWDocID id NOT NULL,
-        PRIMARY KEY ((Year), Month, Day, Kind, Number)
-    ) AS RESULT OF vrestaurant.UpdateSalesPerDay;
+        Article text,
+        -- can be f.e. 0.34
+        Quantity float32, 
+        Price int64,
+        Vat float64,
+        VatPercent float64,
+        PaymentType ref(PaymentTypes) NOT NULL,
+        PRIMARY KEY (Year, Month, Day, Number)
+    ) AS RESULT OF UpdateSalesReport;
 );    
