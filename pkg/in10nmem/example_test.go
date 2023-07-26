d43efb78b040366a367d4cd3cc28604df0ae2100
Maxim Geraskin <m.geraskin@dev.untill.com>
2023-07-26 14:33:55 +0200

    MIT license (#507)

--- pkg/in10nmem/example_test.go
+++ pkg/in10nmem/example_test.go
@@ -1,6 +1,9 @@
 /*
-* Copyright (c) 2023-present unTill Pro, Ltd.
-* @author Maxim Geraskin
+ * Copyright (c) 2023-present unTill Pro, Ltd.
+ * @author Maxim Geraskin
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree. 
  */
 
 package in10nmem_test
