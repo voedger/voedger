f2c54dd9698575f3fe91e810909b6b2b6bd90b2a
Nikolay <mail2nnv@gmail.com>
2023-07-29 15:53:54 +0300

    appdef example

---
+++ pkg/appdef/example_test.go
@@ -0,0 +1,102 @@
+/*
+ * Copyright (c) 2021-present Sigma-Soft, Ltd.
+ */
+
+package appdef_test
+
+import (
+	"fmt"
+
+	"github.com/voedger/voedger/pkg/appdef"
+)
+
+func Example() {
+
+	var app appdef.IAppDef
+	docName, recName := appdef.NewQName("test", "doc"), appdef.NewQName("test", "rec")
+
+	// how to build AppDef with CDoc
+	{
+		appDef := appdef.New()
+
+		doc := appDef.AddCDoc(docName)
+		doc.
+			AddField("f1", appdef.DataKind_int64, true).
+			AddField("f2", appdef.DataKind_string, false)
+		rec := appDef.AddCRecord(recName)
+
+		doc.AddContainer("rec", recName, 0, appdef.Occurs_Unbounded)
+
+		rec.
+			AddField("f1", appdef.DataKind_int64, true).
+			AddField("f2", appdef.DataKind_string, false)
+
+		if a, err := appDef.Build(); err == nil {
+			app = a
+		} else {
+			panic(err)
+		}
+	}
+
+	// how to inspected builded AppDef with CDoc
+	{
+		fmt.Printf("%d definitions\n", app.DefCount())
+
+		// how to find def by name
+		def := app.Def(docName)
+		fmt.Printf("def %q: %v\n", def.QName(), def.Kind())
+
+		// how to cast def to cdoc
+		d, ok := def.(appdef.ICDoc)
+		fmt.Printf("%q is CDoc: %v\n", d.QName(), ok && (d.Kind() == appdef.DefKind_CDoc))
+
+		// how to find CDoc by name
+		doc := app.CDoc(docName)
+		fmt.Printf("doc %q: %v\n", doc.QName(), doc.Kind())
+
+		// how to inspect doc fields
+		fmt.Printf("doc field count: %v\n", doc.UserFieldCount())
+
+		f1 := doc.Field("f1")
+		fmt.Printf("field %q: kind: %v, required: %v\n", f1.Name(), f1.DataKind(), f1.Required())
+
+		fldCnt := 0
+		doc.Fields(func(f appdef.IField) {
+			fldCnt++
+			if f.IsSys() {
+				fmt.Print("*")
+			} else {
+				fmt.Print(" ")
+			}
+			fmt.Printf("%d. Name: %q, kind: %v, required: %v\n", fldCnt, f.Name(), f.DataKind(), f.Required())
+		})
+
+		// how to inspect doc containers
+		fmt.Printf("doc container count: %v\n", doc.ContainerCount())
+
+		c1 := doc.Container("rec")
+		fmt.Printf("container %q: QName: %q, occurs: %v…%v\n", c1.Name(), c1.QName(), c1.MinOccurs(), c1.MaxOccurs())
+
+		contCnt := 0
+		doc.Containers(func(c appdef.IContainer) {
+			contCnt++
+			fmt.Printf("%d. Name: %q, QName: %q, occurs: %v…%v\n", contCnt, c.Name(), c.QName(), c.MinOccurs(), c.MaxOccurs())
+		})
+	}
+
+	// Output:
+	// 2 definitions
+	// def "test.doc": DefKind_CDoc
+	// "test.doc" is CDoc: true
+	// doc "test.doc": DefKind_CDoc
+	// doc field count: 2
+	// field "f1": kind: DataKind_int64, required: true
+	// *1. Name: "sys.QName", kind: DataKind_QName, required: true
+	// *2. Name: "sys.ID", kind: DataKind_RecordID, required: true
+	// *3. Name: "sys.IsActive", kind: DataKind_bool, required: false
+	//  4. Name: "f1", kind: DataKind_int64, required: true
+	//  5. Name: "f2", kind: DataKind_string, required: false
+	// doc container count: 1
+	// container "rec": QName: "test.rec", occurs: 0…unbounded
+	// 1. Name: "rec", QName: "test.rec", occurs: 0…unbounded
+}
