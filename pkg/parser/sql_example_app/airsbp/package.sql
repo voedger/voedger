adafffb72a91945012afcf19eb4c03a64d11468d
michael-say <smmbox@gmail.com>
2023-08-13 16:35:10 +0200

    SQL: copyright in examples

--- pkg/parser/sql_example_app/airsbp/package.sql
+++ pkg/parser/sql_example_app/airsbp/package.sql
@@ -1,3 +1,6 @@
+/*
+* Copyright (c) 2023-present unTill Pro, Ltd.
+*/
 SCHEMA airsbp;
 
 TYPE TypeWithName (
--- pkg/parser/sql_example_app/vrestaurant/package.sql
+++ pkg/parser/sql_example_app/vrestaurant/package.sql
@@ -1,3 +1,6 @@
+/*
+* Copyright (c) 2023-present unTill Pro, Ltd.
+*/
 SCHEMA vrestaurant;
 
 -- TABLE BOEntity : is an Abstract base data struct for many CDOC tables
