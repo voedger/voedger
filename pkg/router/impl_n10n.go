2a5921dd32a35aa2083b023429408af6eef05a1b
alisher <alisher.nurmanov@inbox.ru>
2023-08-23 19:17:23 +0600

    Revert "an experiment"
    This reverts commit 4e8f85182077c77495574d278301d3c28b71663e.

--- pkg/router/impl_n10n.go
+++ pkg/router/impl_n10n.go
@@ -65,6 +65,7 @@ func (s *httpService) subscribeAndWatchHandler() http.HandlerFunc {
 			logger.Error("failed to write created channel id to client:", err)
 			return
 		}
+		flusher.Flush()
 		for _, projection := range urlParams.ProjectionKey {
 			err = s.n10n.Subscribe(channel, projection)
 			if err != nil {
@@ -73,7 +74,6 @@ func (s *httpService) subscribeAndWatchHandler() http.HandlerFunc {
 				return
 			}
 		}
-		flusher.Flush()
 		ch := make(chan in10nmem.UpdateUnit)
 		go func() {
 			defer close(ch)
