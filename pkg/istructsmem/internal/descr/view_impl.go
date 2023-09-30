6de22eb672e31066afc134eb271da102ce4f0937
Nikolay <mail2nnv@gmail.com>
2023-09-30 18:26:42 +0300

    istructsmem/descr

---
+++ pkg/istructsmem/internal/descr/view_impl.go
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2021-present Sigma-Soft, Ltd.
+ * @author: Nikolay Nikitin
+ */
+
+package descr
+
+import "github.com/voedger/voedger/pkg/appdef"
+
+func newView() *View {
+	return &View{
+		Key:   Key{},
+		Value: make([]*Field, 0),
+	}
+}
+
+func (v *View) read(view appdef.IView) {
+	v.Comment = readComment(view)
+
+	v.Name = view.QName()
+
+	v.Key.read(view.Key())
+
+	view.Value().Fields(func(field appdef.IField) {
+		f := newField()
+		f.read(field)
+		v.Value = append(v.Value, f)
+	})
+}
+
+func (k *Key) read(key appdef.IViewKey) {
+	key.Partition().Fields(func(field appdef.IField) {
+		f := newField()
+		f.read(field)
+		k.Partition = append(k.Partition, f)
+	})
+	key.ClustCols().Fields(func(field appdef.IField) {
+		f := newField()
+		f.read(field)
+		k.ClustCols = append(k.ClustCols, f)
+	})
+}
