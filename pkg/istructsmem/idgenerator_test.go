1ecdb1c3aa75f0430837178c0ebfeba5e7752f02
Denis Gribanov <instance6@gmail.com>
2023-10-04 17:04:54 +0300

    go on

--- pkg/istructsmem/idgenerator_test.go
+++ pkg/istructsmem/idgenerator_test.go
@@ -32,6 +32,7 @@ func TestID(t *testing.T) {
 	recID = istructs.RecordID(5000000333)
 	log.Println(recID.BaseRecordID())
 	log.Println(istructs.NewCDocCRecordID(recID.BaseRecordID()))
+	log.Println(istructs.NewRecordID(istructs.NullRecordID))
 }
 
 func TestIDGenerator(t *testing.T) {
@@ -82,20 +83,17 @@ func TestIDGenerator(t *testing.T) {
 			storageID, err := idGen.NextID(1, appDef.Type(qName))
 			require.NoError(err)
 
-			idGen.UpdateOnSync(storageID, appDef.Type(qName))
+			idGen.UpdateOnSync(storageID+1, appDef.Type(qName))
 			storageIDNew, err := idGen.NextID(1, appDef.Type(qName))
 			require.NoError(err)
-			require.Equal(storageID+1, storageIDNew)
+			require.Equal(storageID+2, storageIDNew)
 
 			idGen.UpdateOnSync(storageIDNew+100, appDef.Type(qName))
 			storageIDNew, err = idGen.NextID(1, appDef.Type(qName))
 			require.NoError(err)
-			require.Equal(storageID+102, storageIDNew)
-
-			idGen.UpdateOnSync(storageIDNew-1, appDef.Type(qName))
-			storageIDNew, err = idGen.NextID(1, appDef.Type(qName))
-			require.NoError(err)
 			require.Equal(storageID+103, storageIDNew)
+
+			require.Panics(func() { idGen.UpdateOnSync(storageIDNew-1, appDef.Type(qName)) })
 		}
 	})
 }
@@ -103,6 +101,7 @@ func TestIDGenerator(t *testing.T) {
 // https://github.com/voedger/voedger/issues/688
 // 9999999999 ID causes next IDs collision
 func TestIDGenCollision(t *testing.T) {
+	t.Skip("fixed already. The test is kept as the problem description. Will work on commit e.g. https://github.com/voedger/voedger/commit/cbf1fec92fe1ec25fa17b9897261835c7aa6c017")
 	require := require.New(t)
 	idGen := NewIDGenerator()
 	qNameCDoc := appdef.NewQName(appdef.SysPackage, "cdoc")
