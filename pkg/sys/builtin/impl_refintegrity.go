d8099b0b8f790a1222c5c786cf7bd31607e4ac70
Denis Gribanov <instance6@gmail.com>
2023-10-05 10:12:21 +0300

    go on

--- pkg/sys/builtin/impl_refintegrity.go
+++ pkg/sys/builtin/impl_refintegrity.go
@@ -27,14 +27,22 @@ func provideRefIntegrityValidation(cfg *istructsmem.AppConfigType) {
 			Func: provideRecordsRegistryProjector(cfg),
 		}
 	})
-	cfg.AddCUDValidators(provideRefIntegrityValidator())
-	cfg.AddE
+	// cfg.AddCUDValidators(provideRefIntegrityValidator())
+	cfg.AddEventValidators(refIntegrityValidator)
+}
+
+func refIntegrityValidator(ctx context.Context, rawEvent istructs.IRawEvent, appStructs istructs.IAppStructs, wsid istructs.WSID) error {
+	if coreutils.IsDummyWS(wsid) || rawEvent.QName() == QNameCommandInit {
+		return nil
+	}
+	argType := appStructs.AppDef().Type(rawEvent.ArgumentObject().QName())
+	// if argType.Kind() == appdef.TypeKind_ODoc || argType.Kind() == ORecord
 }
 
 func provideRecordsRegistryProjector(cfg *istructsmem.AppConfigType) func(event istructs.IPLogEvent, st istructs.IState, intents istructs.IIntents) (err error) {
 	return func(event istructs.IPLogEvent, st istructs.IState, intents istructs.IIntents) (err error) {
 		argType := cfg.AppDef.Type(event.ArgumentObject().QName())
-		if  argType.Kind() != appdef.TypeKind_ODoc && argType.Kind() != appdef.TypeKind_ORecord {
+		if argType.Kind() != appdef.TypeKind_ODoc && argType.Kind() != appdef.TypeKind_ORecord {
 			return nil
 		}
 
@@ -138,3 +146,60 @@ func CheckRefIntegrity(obj istructs.IRowReader, appStructs istructs.IAppStructs,
 		return fmt.Errorf("%w: record ID %d referenced by %s.%s does not exist", ErrReferentialIntegrityViolation, actualRefID, qName, refField.Name())
 	})
 }
+
+// func CheckRefIntegrity(obj istructs.IRowReader, appStructs istructs.IAppStructs, wsid istructs.WSID) (err error) {
+// 	appDef := appStructs.AppDef()
+// 	qName := obj.AsQName(appdef.SystemField_QName)
+// 	t := appDef.Type(qName)
+// 	fields, ok := t.(appdef.IFields)
+// 	if !ok {
+// 		return nil
+// 	}
+// 	return iterate.ForEachError(fields.RefFields, func(refField appdef.IRefField) error {
+// 		actualRefID := obj.AsRecordID(refField.Name())
+// 		if actualRefID == istructs.NullRecordID || actualRefID.IsRaw() {
+// 			return nil
+// 		}
+// 		allowedTargetQNames := refField.Refs()
+
+// 		refToRPossible := len(allowedTargetQNames) == 0
+// 		refToOPossible := len(allowedTargetQNames) == 0
+// 		for _, allowedTargetQName := range allowedTargetQNames {
+// 			targetType := appDef.Type(allowedTargetQName)
+// 			switch targetType.Kind() {
+// 			case appdef.TypeKind_ODoc, appdef.TypeKind_ORecord:
+// 				refToOPossible = true
+// 			default:
+// 				refToRPossible = true
+// 			}
+// 		}
+// 		if refToRPossible {
+// 			actualRefRec, err := appStructs.Records().Get(wsid, true, actualRefID)
+// 			if err != nil {
+// 				// notest
+// 				return err
+// 			}
+// 			if actualRefRec.QName() != appdef.NullQName {
+// 				if len(allowedTargetQNames) > 0 && !slices.Contains(allowedTargetQNames, actualRefRec.QName()) {
+// 					return fmt.Errorf("%w: record ID %d referenced by %s.%s is of QName %s whereas %v QNames are only allowed", ErrReferentialIntegrityViolation,
+// 						actualRefID, qName, refField.Name(), actualRefRec.QName(), refField.Refs())
+// 				}
+// 				return nil
+// 			}
+// 		}
+// 		if refToOPossible {
+// 			kb := appStructs.ViewRecords().KeyBuilder(QNameViewORecordsRegistry)
+// 			kb.PutRecordID(field_ID, actualRefID)
+// 			kb.PutInt32(field_Dummy, 1)
+// 			_, err := appStructs.ViewRecords().Get(wsid, kb)
+// 			if err == nil {
+// 				return nil
+// 			}
+// 			if !errors.Is(err, istructsmem.ErrRecordNotFound) {
+// 				// notest
+// 				return err
+// 			}
+// 		}
+// 		return fmt.Errorf("%w: record ID %d referenced by %s.%s does not exist", ErrReferentialIntegrityViolation, actualRefID, qName, refField.Name())
+// 	})
+// }
