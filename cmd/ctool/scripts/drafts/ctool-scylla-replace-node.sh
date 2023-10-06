fd2fe6a65fb1d6044290c5397551340294f60e80
AlekseiPonomarev <paa@sigma-soft.spb.ru>
2023-10-06 18:32:59 +0300

    misc

--- cmd/ctool/scripts/drafts/ctool-scylla-replace-node.sh
+++ cmd/ctool/scripts/drafts/ctool-scylla-replace-node.sh
@@ -11,7 +11,9 @@
 #   - Remove lost node from swarm cluster
 #   - Prepare updated scylla stack
 #   - deploy stack
-#   - add label to new node, to place service 
+#   - add label to new node, to place service
+#   - add node to back seed list and restart service
+#   - rolling restart db cluster
 
 set -euo pipefail
 
@@ -24,48 +26,64 @@ fi
 
 SSH_USER=$LOGNAME
 SSH_OPTIONS='-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o LogLevel=ERROR'
+STACK="DBDockerStack"
+
 
 MANAGER=$3
 REPLACED_NODE_NAME=$(getent hosts "$2" | awk '{print $2}')
 
 wait_for_scylla() {
-    local ip_address=$1
-    echo "Working with $ip_address"
-    local count=0
+  local ip_address=$1
+  echo "Working with $ip_address"
+  local count=0
 
-    while [ $count -lt 100 ]; do
-        if [ "$(ssh "$SSH_OPTIONS" "$SSH_USER"@"$ip_address" docker exec '$(docker ps -qf name=scylla)' nodetool status | grep -c '^UN\s')" -eq 3 ]; then
-            echo "Scylla initialization success"
-            return 0
-        fi
-        echo "Still waiting for Scylla initialization.."
-        sleep 5
-        count=$((count+1))
-    done
-    if [ $count -eq 100 ]; then
-        echo "Scylla initialization timed out."
-        return 1
+  while [ $count -lt 100 ]; do
+    if [ "$(ssh "$SSH_OPTIONS" "$SSH_USER"@"$ip_address" docker exec '$(docker ps -qf name=scylla)' nodetool status | grep -c '^UN\s')" -eq 3 ]; then
+      echo "Scylla initialization success"
+      return 0
     fi
+    echo "Still waiting for Scylla initialization.."
+    sleep 5
+    count=$((count+1))
+  done
+  if [ $count -eq 100 ]; then
+    echo "Scylla initialization timed out."
+    return 1
+  fi
 }
 
-./docker-install.sh $2
+./docker-install.sh "$2"
+./swarm-add-node.sh "$MANAGER" "$2"
+./db-node-prepare.sh "$2"
+./db-bootstrap-prepare.sh "$1" "$2"
+./swarm-rm-node.sh "$MANAGER" "$1"
 
-./swarm-add-node.sh $MANAGER $2
+seed_list() {
+  local node=$1
+  local operation=$2
 
-./db-node-prepare.sh $2
+  service_label=$(./db-stack-update.sh "$node" "$operation" | tail -n 1)
+  < ./docker-compose.yml ssh "$SSH_OPTIONS" "$SSH_USER"@"$node" 'cat > ~/docker-compose.yml'
+  ssh "$SSH_OPTIONS" "$SSH_USER"@"$node" "docker stack deploy --compose-file ~/docker-compose.yml DBDockerStack"
+  ./swarm-set-label.sh "$MANAGER" "$node" "type" "$service_label"
+}
 
-./db-bootstrap-prepare.sh $1 $2
+seed_list "$REPLACED_NODE_NAME" remove
+  wait_for_scylla "$REPLACED_NODE_NAME"
 
-./swarm-rm-node.sh $MANAGER $1
+seed_list "$REPLACED_NODE_NAME" add
+  wait_for_scylla "$REPLACED_NODE_NAME"
 
-service_label=$(./db-stack-update.sh "$REPLACED_NODE_NAME" | tail -n 1)
+db_rolling_restart() {
+  local compose_file="$1"
+  local services=()                                                   
+  mapfile -t services < <(yq r "$compose_file" services.* -j | jq -r 'keys[]')
 
-cat ./docker-compose.yml | ssh $SSH_OPTIONS $SSH_USER@$2 'cat > ~/docker-compose.yml'
-
-ssh $SSH_OPTIONS $SSH_USER@$2 "docker stack deploy --compose-file ~/docker-compose.yml DBDockerStack"
-
-./swarm-set-label.sh $MANAGER $2 "type" $service_label
-
-wait_for_scylla "$2"
+  for service in "${services[@]}"; do
+    echo "Restart service: ${STACK}_${service}"
+    docker service update --force "$STACK"_"$service"
+    wait_for_scylla "$REPLACED_NODE_NAME"
+  done
+}
 
 set +x
--- cmd/ctool/scripts/drafts/db-stack-update.sh
+++ cmd/ctool/scripts/drafts/db-stack-update.sh
@@ -19,24 +19,45 @@ fi
 #lost_node=$1
 #new_node=$2
 
-function modify_clp {
- # $1 - cluster node name
- # $2 - command
+function remove_seed {
+# $1 - cluster node name
+# $2 - command
 
- # $1 - lost node
- # $2 - new_node
- # $3 - input command looks like "command: --seeds 10.0.0.12,10.0.0.13,10.0.0.14 --listen-address 0.0.0.0 --broadcast-address 10.0.0.12 --broadcast-rpc-address 10.0.0.12"
+# $1 - lost node
+# $2 - new_node
+# $3 - input command looks like "command: --seeds 10.0.0.12,10.0.0.13,10.0.0.14 --listen-address 0.0.0.0 --broadcast-address 10.0.0.12 --broadcast-rpc-address 10.0.0.12"
 
-  # Remove the lost_node from the seeds list
-  seeds=$(echo $2 | sed "s/\(--seeds [^ ,]*,\)\?\(,\)\?$1\(,\)\?/\1/")
+# Remove the lost_node from the seeds list
+local updated_command
+updated_command=$(echo $2 | sed "s/\(--seeds [^ ,]*,\)\?\(,\)\?$1\(,\)\?/\1/")
 
-  # Replace the IP address in broadcast-address and broadcast-rpc-address with new_node
-  # output_cmd=$(echo "$seeds" | sed -E "s/--broadcast-address[[:space:]]+$1/--broadcast-address $2/; s/--broadcast-rpc-address[[:space:]]+$1/--broadcast-rpc-address $2/;")
+# Replace the IP address in broadcast-address and broadcast-rpc-address with new_node
+# output_cmd=$(echo "$seeds" | sed -E "s/--broadcast-address[[:space:]]+$1/--broadcast-address $2/; s/--broadcast-rpc-address[[:space:]]+$1/--broadcast-rpc-address $2/;")
 
-  # echo "$output_cmd"
-  echo "$seeds"
+# echo "$output_cmd"
+echo "$updated_command"
 }
 
+function add_seed() {
+# $1 - cluster node name
+# $2 - command
+local updated_command
+local new_seed=$1
+local command=$2
+local current_seeds
+local updated_seeds
+
+# Extract current list of seeds
+current_seeds=$(echo "$command" | grep -oP "(?<=--seeds )[^ ]*")
+
+# Add new seed to the list
+updated_seeds="${current_seeds},${new_seed}"
+
+# replace seed list in command
+updated_command=$(echo "$command" | sed "s/\(--seeds [^ ]* \)/--seeds ${updated_seeds} /")
+
+echo "$updated_command"
+}
 
 # Find the service name that matches the lost_node IP address
 service_name=$(yq e '.services | to_entries | .[] | select(.value.healthcheck.test[] | contains("'$1'")) | .key' docker-compose.yml)
@@ -53,7 +74,20 @@ echo "Updating scylla command '$scy_cmd'"
 
 # new_scy_cmd=$(modify_clp $lost_node $new_node "$scy_cmd")
 
-new_scy_cmd=$(modify_clp $1 "$scy_cmd")
+case "$2" in
+  "remove")
+    new_scy_cmd=$(remove_seed "$1" "$scy_cmd")
+    ;;
+  "add")
+    new_scy_cmd=$(add_seed "$1" "$scy_cmd")
+    ;;
+  *)
+    echo "Usage: $0 <cluster node name> <remove|add>"
+    exit 1
+    ;;
+esac
+
+#new_scy_cmd=$(remove_seed "$1" "$scy_cmd")
 echo "With new command '$new_scy_cmd'"
 
 export new_scy_cmd; yq eval --inplace --prettyPrint '.services."'$service_name'".command = strenv(new_scy_cmd)' docker-compose.yml
