603605d51835955026f0c7f679730272f7e019d4
alisher <alisher.nurmanov@inbox.ru>
2023-10-25 01:47:44 +0600

    go on

--- pkg/parser/utils.go
+++ pkg/parser/utils.go
@@ -6,14 +6,18 @@
 package parser
 
 import (
+	"embed"
 	"fmt"
 	"io/fs"
 	"os"
+	"path"
 	"path/filepath"
 	"reflect"
 	"strings"
 
 	"github.com/voedger/voedger/pkg/appdef"
+	"github.com/voedger/voedger/pkg/apps"
+	"github.com/voedger/voedger/pkg/extensionpoints"
 )
 
 func extractStatement(s any) interface{} {
@@ -351,4 +355,52 @@ func (r *PathReader) ReadDir(name string) ([]os.DirEntry, error) {
 
 func (r *PathReader) ReadFile(name string) ([]byte, error) {
 	return os.ReadFile(filepath.Join(r.rootPath, name))
+}
+
+func readFileSchemaAST(dir string, fsi embed.FS) (fileSchemasAST []*FileSchemaAST, err error) {
+	dirEntries, err := fsi.ReadDir(".")
+	if err != nil {
+		return nil, err
+	}
+	for _, dirEntry := range dirEntries {
+		fileName := dirEntry.Name()
+		sqlContent, err := fsi.ReadFile(fileName)
+		if err != nil {
+			return nil, err
+		}
+		fullFilePath := path.Join(dir, fileName)
+
+		fileSchemaAST, err := ParseFile(fullFilePath, string(sqlContent))
+		if err != nil {
+			return nil, err
+		}
+		fileSchemasAST = append(fileSchemasAST, fileSchemaAST)
+	}
+	return
+}
+
+func ReadPackageSchemaAST(ep extensionpoints.IExtensionPoint) (packageSchemaASTs []*PackageSchemaAST, err error) {
+	epSqlFiles := ep.ExtensionPoint(apps.EPSqlFiles)
+	epSqlFiles.Iterate(func(eKey extensionpoints.EKey, value interface{}) {
+		filesSchemasASTs := make([]*FileSchemaAST, 0)
+		qualifiedPackageName, _ := eKey.(string)
+		epPackageSql := value.(extensionpoints.IExtensionPoint)
+		epPackageSql.Iterate(func(eKey extensionpoints.EKey, value interface{}) {
+			dirAndPackageName := eKey.(string)
+			dir := filepath.Dir(dirAndPackageName)
+
+			fsi, _ := value.(embed.FS)
+			fileSchemaASTs, err := readFileSchemaAST(dir, fsi)
+			if err != nil {
+				panic(err)
+			}
+			filesSchemasASTs = append(filesSchemasASTs, fileSchemaASTs...)
+		})
+		packageSchemaAST, err := BuildPackageSchema(qualifiedPackageName, filesSchemasASTs)
+		if err != nil {
+			panic(err)
+		}
+		packageSchemaASTs = append(packageSchemaASTs, packageSchemaAST)
+	})
+	return
 }
