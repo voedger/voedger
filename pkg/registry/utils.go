0b8b67adc3a320b4b9e6d624764cc848b6dc9b2d
Denis Gribanov <instance6@gmail.com>
2023-10-27 17:39:20 +0300

    misc

--- pkg/registry/utils.go
+++ pkg/registry/utils.go
@@ -67,10 +67,10 @@ func GetCDocLogin(login string, st istructs.IState, appWSID istructs.WSID, appNa
 		return nil, doesLoginExist, err
 	}
 	kb.PutRecordID(state.Field_ID, cdocLoginID)
-	logger.Info(4)
+	logger.Info("4")
 	cdocLogin, err = st.MustExist(kb)
 	if err != nil {
-		logger.Error(4, err)
+		logger.Error("4", err)
 	}
 	return
 }
