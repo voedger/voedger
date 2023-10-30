c9fe5bf04e2769e78cf92286169012e763c73401
maxim-ge <maxim.ge@gmail.com>
2023-10-30 17:51:16 +0100

    fix CI

---
+++ cmd/main_test.go
@@ -0,0 +1,14 @@
+package main
+
+// Fake test, have to be removed after CI is fixed (go test ./cmd... -race -coverprofile=coverage.txt -covermode=atomic -coverpkg=./cmd... -short)
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestSomething(t *testing.T) {
+	require := require.New(t)
+	require.NotNil(t)
+}
