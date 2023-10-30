49cbb582e9715f6792eea99126550c20e4a22cda
maxim-ge <maxim.ge@gmail.com>
2023-10-30 17:54:45 +0100

    works

--- cmd/main_test.go
+++ cmd/main_test.go
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2023-present unTill Software Development Group B. V.
+ * @author Maxim Geraskin
+ */
+
 package main
 
 // Fake test, have to be removed after CI is fixed (go test ./cmd... -race -coverprofile=coverage.txt -covermode=atomic -coverpkg=./cmd... -short)
