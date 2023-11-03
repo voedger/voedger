2df68d11075d728f9f9dc162605f5a8d438c2608
AlekseiPonomarev <paa@sigma-soft.spb.ru>
2023-11-03 14:55:09 +0300

    misc

--- cmd/ctool/scripts/drafts/swarm-init.sh
+++ cmd/ctool/scripts/drafts/swarm-init.sh
@@ -18,6 +18,9 @@ if [ "$#" -lt 1 ]; then
   exit 1
 fi
 
+# Define the desired subnet
+NEW_SUBNET="192.168.0.0/24"
+NEW_GATEWAY="192.168.0.1"
 swarm_manager_ip=$(getent hosts "$1" | awk '{print $1}')
 
 SSH_USER=$LOGNAME
@@ -31,3 +34,26 @@ fi
 
 MANAGER_TOKEN=$(ssh $SSH_OPTIONS $SSH_USER@$1 docker swarm join-token manager | grep -oP "SWMTKN-\S+")
 echo $MANAGER_TOKEN > manager.token
+
+
+# Get the current subnet of the ingress network
+CURRENT_SUBNET=$(docker network inspect -f '{{range .IPAM.Config}}{{.Subnet}}{{end}}' ingress)
+
+# Check if the current subnet matches the desired subnet
+if [ "$CURRENT_SUBNET" == "10.0.0.0/24" ]; then
+    # Remove the existing ingress network
+  docker network rm ingress
+
+  # Create a new ingress network with the desired subnet
+  docker network create \
+  --driver overlay \
+  --ingress \
+  --subnet=$NEW_SUBNET \
+  --gateway=$NEW_GATEWAY \
+  --opt com.docker.network.driver.overlay.vxlanid_list=4096 \
+  ingress
+
+    echo "Ingress network recreated with subnet: $NEW_SUBNET"
+else
+    echo "Ingress network subnet is not 10.0.0.0/24. No action taken."
+fi
