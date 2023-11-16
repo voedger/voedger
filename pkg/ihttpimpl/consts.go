bed2f23a464e6b17c1fc4a9da7c5538a6e64abb9
alisher <alisher.nurmanov@inbox.ru>
2023-11-16 14:07:59 +0600

    go on

--- pkg/ihttpimpl/consts.go
+++ pkg/ihttpimpl/consts.go
@@ -8,15 +8,6 @@ package ihttpimpl
 import "time"
 
 const (
-	NumOfAPIProcessors       = 1
-	APIChannelBufferSize     = 10
 	defaultReadHeaderTimeout = time.Second
 	staticPath               = "/static/"
-)
-
-const (
-	StatusClientClosedRequest  = 499
-	ResponseChannelBufferSize  = 1
-	MaxNumOfConcurrentRequests = 100
-	ReadWriteTimeout           = 5 * time.Second
 )
