fa4cb2b3aa1404d83c32f19f7da1eaba5375224f
alisher <alisher.nurmanov@inbox.ru>
2023-11-22 22:06:26 +0600

    go on

---
+++ cmd/vpm/const.go
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2020-present unTill Pro, Ltd.
+ * @author Alisher Nurmanov
+ */
+
+package main
+
+const (
+	testAQN = "github.com/untillpro/vpm/test"
+)
---
+++ cmd/vpm/main.go
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2022-present unTill Pro, Ltd.
+ * @author Alisher Nurmanov
+ */
+
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"os"
+
+	"github.com/untillpro/goutils/cobrau"
+)
+
+//go:embed version
+var version string
+
+func main() {
+	if err := execRootCmd(os.Args, version); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func execRootCmd(args []string, ver string) error {
+	rootCmd := cobrau.PrepareRootCmd(
+		"vpm",
+		"voedger package manager",
+		args,
+		ver,
+		newCompileCmd(),
+	)
+
+	return cobrau.ExecCommandAndCatchInterrupt(rootCmd)
+}
