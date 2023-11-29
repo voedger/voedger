6029b7dd4005b511cfcd57fe7aeb97028907b5a6
Daniil Solovyov <solovyov.daniil@gmail.com>
2023-11-29 09:48:57 +0100

    coreutils: export constant (#1049)
    https://dev.untill.com/projects/#!668750
    
    Co-authored-by: sda <d.solovyov@dev.untill.com>

--- pkg/utils/email.go
+++ pkg/utils/email.go
@@ -8,15 +8,15 @@ package coreutils
 import "strings"
 
 func IsValidEmailTemplate(emailTemplate string) bool {
-	if !strings.HasPrefix(emailTemplate, emailTemplatePrefix_Text) && !strings.HasPrefix(emailTemplate, emailTemplatePrefix_Resource) {
+	if !strings.HasPrefix(emailTemplate, EmailTemplatePrefix_Text) && !strings.HasPrefix(emailTemplate, emailTemplatePrefix_Resource) {
 		return false
 	}
 	return true
 }
 
 func TruncateEmailTemplate(emailTemplate string) string {
-	if strings.HasPrefix(emailTemplate, emailTemplatePrefix_Text) {
-		return emailTemplate[len(emailTemplatePrefix_Text):]
+	if strings.HasPrefix(emailTemplate, EmailTemplatePrefix_Text) {
+		return emailTemplate[len(EmailTemplatePrefix_Text):]
 	}
 	return emailTemplate[len(emailTemplatePrefix_Resource):]
 }
