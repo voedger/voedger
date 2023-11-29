8fa11b113c01c84f8937b48a66495f3798bf1bbc
alisher <alisher.nurmanov@inbox.ru>
2023-11-30 05:57:08 +0600

    go on

--- cmd/vpm/test/mypkg1/schema1.sql
+++
@@ -1,7 +0,0 @@
-TABLE MyTable1 INHERITS ODoc (
-    myfield1 int32 NOT NULL
-);
-
-TYPE mytype (
-    field text NOT NULL
-);
--- cmd/vpm/test/mypkg3/schema3.sql
+++
@@ -1,5 +0,0 @@
-IMPORT SCHEMA 'github.com/voedger/voedger/pkg/registry';
-
-TABLE MyTable3 INHERITS ODoc (
-    MyField ref(registry.Login) NOT NULL
-);
