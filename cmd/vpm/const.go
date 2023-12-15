d0b8a5bc1463c9a98d87238f0d57f68c5244dcc5
alisher <alisher.nurmanov@inbox.ru>
2023-12-15 09:56:28 +0600

    go on

--- cmd/vpm/const.go
+++ cmd/vpm/const.go
@@ -6,18 +6,12 @@
 package main
 
 const (
-	dummyAppName                = "dummyApp"
-	sysQPN                      = "github.com/voedger/voedger/pkg/sys"
-	sysSchemaSqlFileName        = "sys.sql"
-	baselineDirName             = "baseline_schemas"
-	pkgDirName                  = "pkg"
-	defaultPermissions          = 0766
-	baselineInfoFileName        = "baseline.json"
-	timestampFormat             = "20060102150405"
-	ignoreFlagWithSpace         = "--ignore "
-	ignoreFlagWithEqual         = "--ignore="
-	changeDirFlagWithSpace      = "--change-dir "
-	changeDirFlagWithEqual      = "--change-dir="
-	changeDirShortFlagWithSpace = "-C "
-	changeDirShortFlagWithEqual = "-C="
+	dummyAppName         = "dummyApp"
+	sysQPN               = "github.com/voedger/voedger/pkg/sys"
+	sysSchemaSqlFileName = "sys.sql"
+	baselineDirName      = "baseline_schemas"
+	pkgDirName           = "pkg"
+	defaultPermissions   = 0766
+	baselineInfoFileName = "baseline.json"
+	timestampFormat      = "20060102150405"
 )
