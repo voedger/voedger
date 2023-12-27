cfbe3055a7382e0a7dc51880dc5d0c645d77c2b4
Nikolay <mail2nnv@gmail.com>
2023-12-27 12:14:27 +0300

    magic consts in test

--- pkg/processors/command/impl_test.go
+++ pkg/processors/command/impl_test.go
@@ -617,6 +617,12 @@ func replyBadRequest(bus ibus.IBus, sender interface{}, message string) {
 }
 
 func setUp(t *testing.T, prepareAppDef func(appDef appdef.IAppDefBuilder), cfgFuncs ...func(*istructsmem.AppConfigType)) testApp {
+
+	var (
+		appName istructs.AppQName    = istructs.AppQName_untill_airs_bp
+		partID  istructs.PartitionID = 1
+	)
+
 	if coreutils.IsDebug() {
 		testTimeout = time.Hour
 	}
@@ -638,7 +644,7 @@ func setUp(t *testing.T, prepareAppDef func(appDef appdef.IAppDefBuilder), cfgFu
 	}
 
 	// конфиг приложения airs-bp
-	cfg := cfgs.AddConfig(istructs.AppQName_untill_airs_bp, appDef)
+	cfg := cfgs.AddConfig(appName, appDef)
 	for _, cfgFunc := range cfgFuncs {
 		cfgFunc(cfg)
 	}
@@ -646,13 +652,13 @@ func setUp(t *testing.T, prepareAppDef func(appDef appdef.IAppDefBuilder), cfgFu
 	appStructsProvider := istructsmem.Provide(cfgs, iratesce.TestBucketsFactory,
 		payloads.ProvideIAppTokensFactory(itokensjwt.TestTokensJWT()), appStorageProvider)
 
-	// тут надо подготовить appparts, у которого команда будет заимствовать (borrow) AppStructs
+	// prepare the AppParts to borrow AppStructs
 	appParts, appPartsClean, err := appparts.New(appStructsProvider)
 	require.NoError(t, err)
 	defer appPartsClean()
 
-	appParts.DeployApp(istructs.AppQName_untill_airs_bp, appDef, [cluster.ProcessorKind_Count]int{100, 100, 100}) // команды в тестах идут последовательно, можно указать {1, 0, 0}
-	appParts.DeployAppPartitions(istructs.AppQName_untill_airs_bp, []istructs.PartitionID{1})                     // ?? где взять номера разделов?
+	appParts.DeployApp(appName, appDef, [cluster.ProcessorKind_Count]int{100, 100, 100}) // The commands in the tests go sequentially, you can specify {1, 0, 0}
+	appParts.DeployAppPartitions(appName, []istructs.PartitionID{partID})
 
 	// command processor работает через ibus.SendResponse -> нам нужна реализация ibus
 	var bus ibus.IBus
@@ -672,7 +678,7 @@ func setUp(t *testing.T, prepareAppDef func(appDef appdef.IAppDefBuilder), cfgFu
 			token = strings.TrimPrefix(authHeaders[0], "Bearer ")
 		}
 		command := appDef.Command(cmdQName)
-		icm := NewCommandMessage(ctx, request.Body, appQName, istructs.WSID(request.WSID), sender, 1, command, token, "")
+		icm := NewCommandMessage(ctx, request.Body, appQName, istructs.WSID(request.WSID), sender, partID, command, token, "")
 		serviceChannel <- icm
 	})
 	n10nBroker, n10nBrokerCleanup := in10nmem.ProvideEx2(in10n.Quotas{
@@ -683,13 +689,13 @@ func setUp(t *testing.T, prepareAppDef func(appDef appdef.IAppDefBuilder), cfgFu
 	}, time.Now)
 
 	tokens := itokensjwt.ProvideITokens(itokensjwt.SecretKeyExample, time.Now)
-	appTokens := payloads.ProvideIAppTokensFactory(tokens).New(istructs.AppQName_untill_airs_bp)
+	appTokens := payloads.ProvideIAppTokensFactory(tokens).New(appName)
 	systemToken, err := payloads.GetSystemPrincipalTokenApp(appTokens)
 	require.NoError(t, err)
 	cmdProcessorFactory := ProvideServiceFactory(bus, appParts, time.Now, func(ctx context.Context, partitionID istructs.PartitionID) pipeline.ISyncOperator {
 		return &pipeline.NOOP{}
 	}, n10nBroker, imetrics.Provide(), "vvm", iauthnzimpl.NewDefaultAuthenticator(iauthnzimpl.TestSubjectRolesGetter), iauthnzimpl.NewDefaultAuthorizer(), isecretsimpl.ProvideSecretReader(), cfgs)
-	cmdProcService := cmdProcessorFactory(serviceChannel, 1)
+	cmdProcService := cmdProcessorFactory(serviceChannel, partID)
 
 	go func() {
 		cmdProcService.Run(ctx)
