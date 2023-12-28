0c400427e6b942c0c8fdda2453cc625596545420
Alisher <38488987+anurmanov@users.noreply.github.com>
2023-12-28 21:33:02 +0600

    1165 implement ctool https (#1226)
    * Commit for keeping notes in branch
    
    * go on
    
    * Commit for keeping notes in branch
    
    * go on
    
    * Commit for keeping notes in branch
    
    * go on
    
    * Commit for keeping notes in branch
    
    * go on
    
    * go on
    
    * go on
    
    * Commit for keeping notes in branch
    
    * go on
    
    * Commit for keeping notes in branch
    
    * Commit for keeping notes in branch
    
    * go on
    
    * Commit for keeping notes in branch
    
    * go on
    
    * go on
    
    * go on
    
    * go on

--- pkg/ihttpctl/provide.go
+++ pkg/ihttpctl/provide.go
@@ -11,7 +11,7 @@ import (
 	"github.com/voedger/voedger/pkg/ihttp"
 )
 
-func NewHTTPProcessorController(api ihttp.IHTTPProcessorAPI, staticResources []StaticResourcesType, redirections RedirectRoutes, defaultRedirection DefaultRedirectRoute) (IHTTPProcessorController, error) {
+func NewHTTPProcessorController(api ihttp.IHTTPProcessorAPI, staticResources []StaticResourcesType, redirections RedirectRoutes, defaultRedirection DefaultRedirectRoute, acmeDomains ihttp.AcmeDomains) (IHTTPProcessorController, error) {
 	srs := StaticResourcesType{}
 	for _, sr := range staticResources {
 		for url, fs := range sr {
@@ -24,10 +24,14 @@ func NewHTTPProcessorController(api ihttp.IHTTPProcessorAPI, staticResources []S
 	if len(defaultRedirection) > 1 {
 		panic("default redirection should be single record")
 	}
-	return &httpProcessorController{
+	httpController := &httpProcessorController{
 		api:                api,
 		staticResources:    srs,
 		redirections:       redirections,
 		defaultRedirection: defaultRedirection,
-	}, nil
+	}
+	for _, acmeDomain := range acmeDomains {
+		httpController.api.AddAcmeDomain(acmeDomain)
+	}
+	return httpController, nil
 }
