6b1153d6804493848703a3c7db7cf4f34e6be6b5
Alisher <38488987+anurmanov@users.noreply.github.com>
2024-01-15 13:16:08 +0600

    1282 vpm consistent naming (#1288)

--- cmd/vpm/main.go
+++ cmd/vpm/main.go
@@ -9,7 +9,10 @@ import (
 	_ "embed"
 	"fmt"
 	"os"
+	"strings"
 
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 	"github.com/untillpro/goutils/cobrau"
 )
 
@@ -26,13 +29,43 @@ func main() {
 func execRootCmd(args []string, ver string) error {
 	rootCmd := cobrau.PrepareRootCmd(
 		"vpm",
-		"voedger package manager",
+		"",
 		args,
 		ver,
 		newCompileCmd(),
 		newBaselineCmd(),
 		newCompatCmd(),
 	)
-
+	rootCmd.InitDefaultHelpCmd()
+	rootCmd.InitDefaultCompletionCmd()
+	correctCommandTexts(rootCmd)
 	return cobrau.ExecCommandAndCatchInterrupt(rootCmd)
+}
+
+// correctCommandTexts makes first letter of command and its flags descriptions small
+// works recursively for all subcommands
+func correctCommandTexts(cmd *cobra.Command) {
+	correctCommandFlagTexts(cmd)
+	for _, c := range cmd.Commands() {
+		c.Short = makeFirstLetterSmall(c.Short)
+		correctCommandTexts(c)
+	}
+}
+
+func correctCommandFlagTexts(cmd *cobra.Command) {
+	correctFlagSetTexts(cmd.Flags())
+	correctFlagSetTexts(cmd.PersistentFlags())
+}
+
+func correctFlagSetTexts(fs *pflag.FlagSet) {
+	fs.VisitAll(func(f *pflag.Flag) {
+		f.Usage = makeFirstLetterSmall(f.Usage)
+	})
+}
+
+func makeFirstLetterSmall(s string) string {
+	if len(s) == 0 {
+		return s
+	}
+	return strings.ToLower(s[0:1]) + s[1:]
 }
