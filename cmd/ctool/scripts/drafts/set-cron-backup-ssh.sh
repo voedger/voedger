10812b6143fed92cd94dc3011b33fee9fa2b7ff5
Dmitry Molchanovsky <90905754+dmitrymolchanovky@users.noreply.github.com>
2024-01-16 18:55:22 +0300

    #1027 ctool backup cron '* * /24 * *'(#1296)

---
+++ cmd/ctool/scripts/drafts/set-cron-backup-ssh.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2024 Sigma-Soft, Ltd.
+# @author Dmitry Molchanovsky
+# 
+# writes a database backup task to cron
+# over an ssh connection
+set -euo pipefail
+set -x
+
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <cron schedule time>" 
+  exit 1
+fi
+
+source ./utils.sh
+
+SCHEDULE=$1
+SSH_USER=$LOGNAME
+CRON_HOST_NAME="app-node-1"
+CRON_HOST=$(nslookup ${CRON_HOST_NAME} | awk '/^Address: / { print $2 }')
+
+REMOTE_COMMAND="bash -s \"${SCHEDULE}\""
+
+utils_ssh -t "${SSH_USER}"@"${CRON_HOST}" "${REMOTE_COMMAND}" < set-cron-backup.sh
+
+set +x
