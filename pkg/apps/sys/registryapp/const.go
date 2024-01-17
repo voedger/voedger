7a351169ba21839619414875a070480196f883c8
Nikolay <mail2nnv@gmail.com>
2024-01-17 14:42:07 +0300

    cluster/builtin: default PartsCount()

--- pkg/apps/sys/registryapp/const.go
+++ pkg/apps/sys/registryapp/const.go
@@ -5,9 +5,17 @@
 
 package registryapp
 
-import "embed"
+import (
+	"embed"
+
+	"github.com/voedger/voedger/pkg/cluster"
+)
 
 //go:embed schema.sql
 var registryAppSchemaFS embed.FS
 
 const RegistryAppFQN = "github.com/voedger/voedger/pkg/apps/sys/registryapp"
+
+const DefDeploymentPartsCount = 10
+
+var DefDeploymentEnginePoolSize = cluster.PoolSize(10, 10, 10)
