f3bda7f54f7dd5a0c72c9327bea54d782ffb2051
Denis Gribanov <d.gribanov@dev.untill.com>
2024-01-22 11:27:02 +0300

    go on

---
+++ pkg/utils/deferop_test.go
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2024-present unTill Software Development Group B.V.
+ * @author Denis Gribanov
+ */
+
+package coreutils
+
+import "testing"
+
+func TestDeferOpBasicUsage(t *testing.T) {
+
+}
