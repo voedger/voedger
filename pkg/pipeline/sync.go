a25603846916ca871a94c08a6c7e61101ac5d2f7
maxim-ge <m.geraskin@untill.com>
2024-01-31 20:41:00 +0100

    works

--- pkg/pipeline/sync.go
+++ pkg/pipeline/sync.go
@@ -1,10 +1,7 @@
-/*
-*
-* Copyright (c) 2021-present unTill Pro, Ltd.
-*
-* @author Michael Saigachenko
-*
- */
+// Copyright (c) 2021-present Voedger Authors.
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+// @author Michael Saigachenko
 
 package pipeline
 
