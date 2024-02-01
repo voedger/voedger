f9976cad60c8d0be206b88a3a248cd85df2530c4
Denis Gribanov <instance6@gmail.com>
2024-02-01 12:17:48 +0300

    #1389 eliminate `ToJSON()` from entities of `state` package(#1390)

--- pkg/state/impl_wlog_storage.go
+++ pkg/state/impl_wlog_storage.go
@@ -6,11 +6,9 @@ package state
 
 import (
 	"context"
-	"encoding/json"
 
 	"github.com/voedger/voedger/pkg/appdef"
 	"github.com/voedger/voedger/pkg/istructs"
-	coreutils "github.com/voedger/voedger/pkg/utils"
 )
 
 type wLogStorage struct {
@@ -43,35 +41,9 @@ func (s *wLogStorage) Read(kb istructs.IStateKeyBuilder, callback istructs.Value
 		return callback(
 			&key{data: map[string]interface{}{Field_Offset: offs}},
 			&wLogValue{
-				event:      event,
-				offset:     offs,
-				toJSONFunc: s.toJSON,
+				event:  event,
+				offset: offs,
 			})
 	}
 	return s.eventsFunc().ReadWLog(s.ctx, k.wsid, k.offset, k.count, cb)
-}
-func (s *wLogStorage) toJSON(sv istructs.IStateValue, _ ...interface{}) (string, error) {
-	value := sv.(*wLogValue)
-	obj := make(map[string]interface{})
-	obj["QName"] = value.event.QName().String()
-	obj["ArgumentObject"] = coreutils.ObjectToMap(value.event.ArgumentObject(), s.appDefFunc())
-	cc := make([]map[string]interface{}, 0)
-	value.event.CUDs(func(rec istructs.ICUDRow) {
-		cudRowMap := cudRowToMap(rec, s.appDefFunc)
-		cc = append(cc, cudRowMap)
-	})
-	obj["CUDs"] = cc
-	obj[Field_RegisteredAt] = value.event.RegisteredAt()
-	obj["Synced"] = value.event.Synced()
-	obj[Field_DeviceID] = value.event.DeviceID()
-	obj[Field_SyncedAt] = value.event.SyncedAt()
-	errObj := make(map[string]interface{})
-	errObj["ErrStr"] = value.event.Error().ErrStr()
-	errObj["QNameFromParams"] = value.event.Error().QNameFromParams().String()
-	errObj["ValidEvent"] = value.event.Error().ValidEvent()
-	errObj["OriginalEventBytes"] = value.event.Error().OriginalEventBytes()
-	obj["Error"] = errObj
-	obj[Field_Offset] = value.offset
-	bb, err := json.Marshal(&obj)
-	return string(bb), err
 }
