2d1d1e61f4d97ff98e8e20158d1193ecf639d016
Denis Gribanov <instance6@gmail.com>
2024-02-15 01:31:25 +0300

    #1457 VIT: fix additional non-acomplishing actions in tests(#1458)

--- pkg/sys/it/test_utils.go
+++ pkg/sys/it/test_utils.go
@@ -8,6 +8,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"regexp"
+	"runtime"
 	"testing"
 	"time"
 
@@ -96,7 +97,8 @@ func WaitForInviteState(vit *it.VIT, ws *it.AppWorkspace, inviteID int64, invite
 			break
 		}
 	}
-	vit.T.Fatalf("invite %d is failed achieve the state %d. The last state was %d", inviteID, inviteStatesSeq[len(inviteStatesSeq)-1], actualInviteState)
+	_, file, line, _ := runtime.Caller(1)
+	vit.T.Fatalf("%s:%d: invite %d is failed achieve the state %d. The last state was %d", file, line, inviteID, inviteStatesSeq[len(inviteStatesSeq)-1], actualInviteState)
 }
 
 type joinedWorkspaceDesc struct {
