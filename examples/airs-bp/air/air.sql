a091e8ec3f42be29388f41438ab2cd268c4fd879
Maxim Geraskin <m.geraskin@dev.untill.com>
2024-02-24 20:51:46 +0100

    #1488 examples/airs-bp: tests prototypes(#1526)

---
+++ examples/airs-bp/air/air.sql
@@ -0,0 +1,25 @@
+-- Copyright (c) 2024-present unTill Software Development Group B. V.
+-- @author Maxim Geraskin
+
+IMPORT SCHEMA 'github.com/untillpro/airs-scheme/bp3' AS untill;
+
+WORKSPACE RestaurantWS (
+
+	TABLE ProformaPrinted INHERITS ODoc (
+		Number int32 NOT NULL,
+		UserID ref(untill.untill_users) NOT NULL,
+		Timestamp int64 NOT NULL,
+		BillID ref(untill.bill) NOT NULL
+	);
+
+	VIEW PbillDates (
+		Year int32 NOT NULL,
+		DayOfYear int32 NOT NULL,
+		FirstOffset int64 NOT NULL,
+		LastOffset int64 NOT NULL,
+		PRIMARY KEY ((Year), DayOfYear)
+	) AS RESULT OF FillPbillDates;
+
+	COMMAND Orders(untill.orders);
+	COMMAND Pbill(untill.pbill) RETURNS CmdPBillResult;
+)
