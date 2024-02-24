fcec385a718c3600faa313670f15a7f26c509611
Maxim Geraskin <m.geraskin@dev.untill.com>
2024-02-24 20:51:46 +0100

    #1488 examples/airs-bp: tests prototypes(#1526)

--- examples/airs-bp/air/extwasm/main.go
+++ examples/airs-bp/air/extwasm/main.go
@@ -5,28 +5,87 @@
 
 package main
 
-import "extwasm/schemas"
+import (
+	"extwasm/orm"
+	"time"
+)
 
-func MyFunc() {
+// Command
+func Pbill() {
 
-	// Query air.ProformaPrinted
+	var refBill orm.Ref
+
+	// Query untill.pbill from the ArgumentObject
 	{
-		v := schemas.Air.ProformaPrinted.MustGetValue(schemas.ID(12))
-		println(v.Number())
-		println(v.BillID())
+		pbill := orm.Package_air.Command_Pbill.ArgumentObject()
+
+		// Basic types fields
+		refBill = pbill.Get_id_bill()
+		pbill.Get_id_untill_users()
+
+		// Container
+		pbill_items := pbill.Get_pbill_item()
+		for i := 0; i < pbill_items.Len(); i++ {
+			item := pbill_items.Get(i)
+			item.Get_rowbeg()
+			item.Get_tableno()
+		}
 	}
+
+	// Prepare intent for Package_untill.WDoc_pbill
+	{
+		intent := orm.Package_untill.WDoc_pbill.NewIntent(refBill.ID())
+		intent.Set_close_datetime(time.Now().UnixMicro())
+	}
+
+	// Prepare intent for Package_air.WSingleton_NextNumbers
+	{
+		nextNumberValue, nextNumberOk := orm.Package_air.WSingleton_NextNumbers.QueryValue()
+		var nextNumber int32
+		var intent orm.Intent_WSingleton_air_NextNumbers
+		if !nextNumberOk {
+			nextNumber = 1
+			intent = orm.Package_air.WSingleton_NextNumbers.NewIntent()
+		} else {
+			nextNumber = nextNumberValue.Get_NextPBillNumber()
+			intent = nextNumberValue.NewIntent()
+		}
+		intent.Set_NextPBillNumber(nextNumber + 1)
+	}
+
+}
+
+func MyProjector() {
 
 	// Query air.PbillDates
 	{
-		v := schemas.Air.PbillDates.MustGetValue(2019, 12)
-		println(v.FirstOffset())
-		println(v.LastOffset())
+		v := orm.Package_air.View_PbillDates.MustGetValue(2019, 12)
+		println(v.Get_FirstOffset())
+		println(v.Get_LastOffset())
 	}
 
 	// Query untill.Articles
 	{
-		v := schemas.Untill.Articles.MustGetValue(schemas.ID(12))
-		println(v.Article_number())
+		v := orm.Package_untill.CDoc_articles.MustGetValue(orm.ID(12))
+		println(v.Get_article_number())
 		println(v.Name())
+	}
+
+	// Query air.PbillDates and create intents
+	{
+		{
+			v, ok := orm.Package_air.View_PbillDates.QueryValue(2019, 12)
+			if ok {
+				intent := v.NewIntent()
+				// `Set` is a must to execute naming conflicts with NewIntent()
+				intent.Set_FirstOffset(1)
+				intent.Set_LastOffset(2)
+			}
+		}
+		{
+			intent := orm.Package_air.View_PbillDates.NewIntent(2020, 1)
+			intent.Set_FirstOffset(20)
+			intent.Set_LastOffset(17)
+		}
 	}
 }
---
+++ examples/airs-bp/air/extwasm/main_test.go
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2024-present unTill Software Development Group B. V.
+ * @author Maxim Geraskin
+ */
+
+package main
+
+import (
+	"extwasm/exttest"
+	"extwasm/orm"
+	"testing"
+
+	exttinygo "github.com/voedger/exttinygo"
+)
+
+func Test_Pbill(t *testing.T) {
+
+	// // Should use test name to check appdef and prepare test context
+	exttest.InitTest(t)
+
+	// Prepare first test data
+	{
+		stateIntents := []exttinygo.TIntent{
+			orm.Package_air.View_PbillDates.NewIntent(2020, 1).Set_FirstOffset(20).Set_LastOffset(17).Intent(),
+			orm.Package_air.View_PbillDates.NewIntent(2021, 2).Set_FirstOffset(21).Set_LastOffset(18).Intent(),
+			// exttest.Event.NewIntent().Set_QName("").Intent(),
+		}
+		expectedIntents := []exttinygo.TIntent{
+			orm.Package_air.View_PbillDates.NewIntent(2020, 1).Set_FirstOffset(20).Set_LastOffset(17).Intent(),
+			orm.Package_air.View_PbillDates.NewIntent(2021, 2).Set_FirstOffset(21).Set_LastOffset(18).Intent(),
+		}
+
+		_ = stateIntents
+		_ = expectedIntents
+		// exttest.RunAndCheck(stateIntents, Pbill, expectedIntents)
+
+		{
+			// Test intents
+			{
+
+			}
+		}
+	}
+}
