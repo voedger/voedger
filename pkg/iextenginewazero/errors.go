239402194903af56387d51c32087d3a47913a23e
Michael Saigachenko <m.saigachenko@dev.untill.com>
2024-03-01 16:49:15 +0300

    #1595 iextenginewasm: panic with nil err(#1596)

--- pkg/iextenginewazero/errors.go
+++ pkg/iextenginewazero/errors.go
@@ -16,6 +16,8 @@ var (
 	PanicIncorrectIntent     = "incorrect intent"
 )
 
+var errMemoryOutOfRange = errors.New("memory out of range")
+
 func missingExportedFunction(name string) error {
 	return errors.New("missing exported function: " + name)
 }
