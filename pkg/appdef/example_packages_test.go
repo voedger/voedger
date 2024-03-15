0913587fa8a3832b2d1d3b83d440d3a8f75f0bc7
Nikolay <mail2nnv@gmail.com>
2024-03-15 15:06:43 +0300

    appdef: better names in packages example

--- pkg/appdef/example_packages_test.go
+++ pkg/appdef/example_packages_test.go
@@ -17,8 +17,8 @@ func ExampleIAppDefBuilder_AddPackage() {
 	{
 		appDef := appdef.New()
 
-		appDef.AddPackage("test", "test.com/path")
-		appDef.AddPackage("example", "example.com/path")
+		appDef.AddPackage("test", "test.com/test")
+		appDef.AddPackage("example", "example.com/example")
 
 		if a, err := appDef.Build(); err == nil {
 			app = a
@@ -29,8 +29,8 @@ func ExampleIAppDefBuilder_AddPackage() {
 
 	// how to inspect builded AppDef with packages
 	{
-		fmt.Println(app.PackageLocalName("test.com/path"), app.PackageFullPath("test"))
-		fmt.Println(app.PackageLocalName("example.com/path"), app.PackageFullPath("example"))
+		fmt.Println(app.PackageLocalName("test.com/test"), app.PackageFullPath("test"))
+		fmt.Println(app.PackageLocalName("example.com/example"), app.PackageFullPath("example"))
 
 		fmt.Println(app.PackageLocalNames())
 
@@ -39,15 +39,15 @@ func ExampleIAppDefBuilder_AddPackage() {
 		})
 
 		fmt.Println(app.FullQName(appdef.NewQName("test", "name")))
-		fmt.Println(app.LocalQName(appdef.NewFullQName("example.com/path", "name")))
+		fmt.Println(app.LocalQName(appdef.NewFullQName("example.com/example", "name")))
 	}
 
 	// Output:
-	// test test.com/path
-	// example example.com/path
+	// test test.com/test
+	// example example.com/example
 	// [example test]
-	// example example.com/path
-	// test test.com/path
-	// test.com/path.name
+	// example example.com/example
+	// test test.com/test
+	// test.com/test.name
 	// example.name
 }
