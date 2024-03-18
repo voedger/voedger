31951ef078b1db20b12d9967108ba8f187b8067e
dmitrymolchanovky <mda@sigma-soft.spb.ru>
2024-03-18 17:15:38 +0300

    mon-node-prepare.sh

---
+++ cmd/ctool/scripts/drafts/prometheus-admin-password.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2024 Sigma-Soft, Ltd.
+# @author Dmitry Molchanovsky
+# 
+# 
+# Sets a new password for the admin user in Prometheus
+# on specified remote hosts
+set -euo pipefail
+set -x
+if [ $# -lt 3 ]; then
+    echo "Usage: $0 <password> <hashed password> <host1> [<host2> ...]"
+    exit 1
+fi
+
+source ./utils.sh
+
+NEW_PASSWORD=$1
+HASHED_PASSWORD=$2
+SSH_USER=$LOGNAME
+
+shift 2
+for host in "$@"; do
+    APP_NODE_CONTAINER=$(utils_ssh ${SSH_USER}@${host} "docker ps --format '{{.Names}}' | grep prometheus")
+    if [ -z "$APP_NODE_CONTAINER" ]; then
+        echo "Prometheus container was not found on a host ${host}"
+    else
+        utils_ssh ${SSH_USER}@${host} "sed -i 's/admin:.*/admin: ${HASHED_PASSWORD//\//\\/}/' ~/prometheus/web.yml"
+        echo "Password for admin user in Prometheus on ${host} was successfully changed"
+        
+	if [ host == "app-node-1" ]; then	
+       	  SERVICE_NAME="MonDockerStack_prometheus1"
+	else
+         SERVICE_NAME="MonDockerStack_prometheus2"
+	fi	
+    
+    utils_ssh ${SSH_USER}@${host} "docker service update ${SERVICE_NAME} --force"
+	echo "${SERVICE_NAME} service was updated"
+        
+   fi
+done
+set +x
