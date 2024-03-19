87faa961bfde601b3e678477416cdafae1c241a1
AlekseiPonomarev <paa@sigma-soft.spb.ru>
2024-03-19 14:09:26 +0300

    Add basic auth to grafana as client prometheus

---
+++ cmd/ctool/scripts/drafts/g_ds_update.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2024 Sigma-Soft, Ltd.
+# @author Aleksei Ponomarev
+# 
+# Add new user to grafana
+
+set -euo pipefail
+
+set -x
+
+if [[ $# -ne 4 ]]; then
+  echo "Usage: $0 <app-node> <grafana admin user> <grafana admin password> <new-user-password>"
+  exit 1
+fi
+
+HOST=$1
+# Admin credentials
+ADMIN_USER=$2
+ADMIN_PASSWORD=$3
+DATASOURCE_NAME="Prometheus"
+NEW_BASIC_AUTH_USER="vsvs"
+NEW_BASIC_AUTH_PASSWORD="$4"
+
+# User details
+USER_NAME="voedger"
+USER_LOGIN="voedger"
+USER_PASSWORD="voedger"
+
+
+DsIDbyName() {
+  local name="$1"
+  local resp
+  local url="http://$HOST:3000/api/datasources/name/$name"
+
+  resp=$(curl -s -u "$ADMIN_USER:$ADMIN_PASSWORD" "$url")
+  echo $(echo "$resp" | jq '.id')
+}
+
+
+UPDATE_DS_PAYLOAD=$(cat <<-EOF
+  {
+    "basicAuth": true,
+    "basicAuthUser": "${NEW_BASIC_AUTH_USER}",
+    "secureJsonData": {
+        "basicAuthPassword": "${NEW_BASIC_AUTH_PASSWORD}"
+    }
+  }
+EOF
+)
+
+dsID=$(DsIDbyName "${DATASOURCE_NAME}")
+
+if [ "$dsID" == "null" ]; then
+  echo "Datasource '$DATASOURCE_NAME' not found."
+  exit 1
+fi
+
+http_resp=$(curl -s -o /dev/null -w "%{http_code}" -X PUT -u "${ADMIN_USER}:${ADMIN_PASSWORD}" -H "Content-Type: application/json" -d "$UPDATE_DS_PAYLOAD" "http://$HOST:3000/api/datasources/$dsID")
+
+if [ "$http_resp" -eq 200 ]; then
+    echo "Datasource '${DATASOURCE_NAME}' update with success."
+  else
+    echo "Error update datasource '${DATASOURCE_NAME}'. HTTP: $http_resp."
+    exit 1
+  fi
+exit 0
