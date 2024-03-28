5d6b99ec09727ae8faad2fc67b8665660b5f4a0c
michael-say <smmbox@gmail.com>
2024-03-28 16:24:26 +0100

    iextengine: api for testing

--- pkg/iextengine/testctx/_testdata/basicusage/main.go
+++ pkg/iextengine/testctx/_testdata/basicusage/main.go
@@ -51,35 +51,5 @@ func CalcOrderedItems() {
 	}
 }
 
-// Handle JSON Example
-//
-//export updateSubscriptionProjector
-func updateSubscriptionProjector() {
-	event := ext.MustGetValue(ext.KeyBuilder(ext.StorageEvent, ext.NullEntity))
-
-	if event.AsString("qname") == "air.UpdateSubscription" {
-		json := event.AsValue("arg")
-		subscr := json.AsValue("subscription")
-		customer := json.AsValue("customer")
-		mail := ext.NewValue(ext.KeyBuilder(ext.StorageSendMail, ext.NullEntity))
-		mail.PutString("from", "test@gmail.com")
-		mail.PutString("to", customer.AsString("email"))
-		mail.PutString("body", "Your subscription has been updated. New status: "+subscr.AsString("status"))
-	}
-}
-
-//export incrementProjector
-func incrementProjector() {
-	key := ext.KeyBuilder(ext.StorageView, "mypkg.TestView")
-	key.PutInt32("pk", 1)
-	key.PutInt32("cc", 1)
-	value, exists := ext.QueryValue(key)
-	if !exists {
-		ext.NewValue(key).PutInt32("vv", 1)
-	} else {
-		ext.UpdateValue(key, value).PutInt32("vv", value.AsInt32("vv")+1)
-	}
-}
-
 func main() {
 }
