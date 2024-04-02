e185a0e003e04113fde881699a2dabb7e3c096b1
Nikolay <mail2nnv@gmail.com>
2024-04-02 15:01:59 +0300

    appparts: remove unused fabrics from provide

--- pkg/appparts/provide.go
+++ pkg/appparts/provide.go
@@ -16,26 +16,17 @@ type SyncActualizerFactory = func(istructs.IAppStructs, istructs.PartitionID) pi
 
 // New only for tests where sync actualizer is not used
 func New(structs istructs.IAppStructsProvider) (ap IAppPartitions, cleanup func(), err error) {
-	return NewWithActualizer(
+	return NewWithActualizerWithExtEnginesFactories(
 		structs,
 		func(istructs.IAppStructs, istructs.PartitionID) pipeline.ISyncOperator {
 			return &pipeline.NOOP{}
 		},
-	)
-}
-
-func NewWithActualizer(structs istructs.IAppStructsProvider, actualizer SyncActualizerFactory) (ap IAppPartitions, cleanup func(), err error) {
-	return NewWithActualizerWithExtEnginesFactories(
-		structs,
-		actualizer,
 		iextengine.ExtensionEngineFactories{
 			appdef.ExtensionEngineKind_BuiltIn: iextengine.NullExtensionEngineFactory,
 			appdef.ExtensionEngineKind_WASM:    iextengine.NullExtensionEngineFactory,
 		},
 	)
 }
-
-type ExtensionEngineFactoriesFactory func(istructs.AppQName) iextengine.ExtensionEngineFactories
 
 func NewWithActualizerWithExtEnginesFactories(structs istructs.IAppStructsProvider, actualizer SyncActualizerFactory,
 	eef iextengine.ExtensionEngineFactories) (ap IAppPartitions, cleanup func(), err error) {
