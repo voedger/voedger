514fc4d4c76984708f9af4bf86e1c597da080b2f
AlekseiPonomarev <paa@sigma-soft.spb.ru>
2024-04-04 18:22:38 +0300

    g_user_add.sh - check user is exists

--- cmd/ctool/scripts/drafts/g_user_add.sh
+++ cmd/ctool/scripts/drafts/g_user_add.sh
@@ -30,17 +30,27 @@ CREATE_USER_PAYLOAD="{\"name\":\"${USER_NAME}\", \
 
   API_URL="http://$HOST:3000/api/admin/users"
 
-  # Create user using curl and parse response with jq
-  resp=$(curl -X POST -H "Content-Type: application/json" -d "$CREATE_USER_PAYLOAD" --user "$ADMIN_USER":"$ADMIN_PASSWORD" "$API_URL")
+# Check user already exist
+user_id=$(curl -s -u "$ADMIN_USER:$ADMIN_PASSWORD" "http://$HOST:3000/api/users/lookup?loginOrEmail=$USER_NAME" | jq '.id')
 
-  user_id=$(echo "$resp" | jq -r '.id')
-  message=$(echo "$resp" | jq -r '.message')
 
-  if [ "$user_id" != "null" ]; then
-    echo "User created with ID: $user_id on $HOST. Message: $message"
-  else
-    echo "User creation failed on $HOST. Message: $message"
-    exit 1
-  fi
+# If not found - error
+if [ "$user_id" == "null" ]; then
+    echo "User '$USER_NAME' not found. Create..."
+    # Create user using curl and parse response with jq
+    resp=$(curl -X POST -H "Content-Type: application/json" -d "$CREATE_USER_PAYLOAD" --user "$ADMIN_USER":"$ADMIN_PASSWORD" "$API_URL")
+else
+    exit 0
+fi
+
+user_id=$(echo "$resp" | jq -r '.id')
+message=$(echo "$resp" | jq -r '.message')
+
+if [ "$user_id" != "null" ]; then
+  echo "User created with ID: $user_id on $HOST. Message: $message"
+else
+  echo "User creation failed on $HOST. Message: $message"
+  exit 1
+fi
 
 exit 0
