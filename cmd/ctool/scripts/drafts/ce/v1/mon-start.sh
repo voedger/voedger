f3be9f16057189aa00b713cb0ef9ee1cf7d4a8b1
AlekseiPonomarev <paa@sigma-soft.spb.ru>
2024-04-06 11:47:12 +0300

    misc

---
+++ cmd/ctool/scripts/drafts/ce/v1/mon-start.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2024 Sigma-Soft, Ltd.
+# @author Aleksei Ponomarev
+#
+
+set -euo pipefail
+
+
+if [ -n "${VOEDGER_CE_NODE:-}" ]; then
+    envsubst < ./docker-compose-mon.yml | sudo docker-compose -p MONDockerStack -f - up -d
+else
+   echo "Error deploy monitoring stack. Use export VOEDGER_CE_NODE= <hostname | ipaddress>."  
+   exit 1 
+fi
+
+exit 0
