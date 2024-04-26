a8a07f8c1e06939db6cc85553f470c9607669af8
Nikolay <mail2nnv@gmail.com>
2024-04-26 13:06:42 +0300

    merge files rate* + limit*  —> ratelimit*

--- pkg/appdef/interface_ratelimit.go
+++ pkg/appdef/interface_ratelimit.go
@@ -62,3 +62,32 @@ type IRatesBuilder interface {
 	//   - if period is zero.
 	AddRate(name QName, count RateCount, period RatePeriod, scopes []RateScope, comment ...string)
 }
+
+type ILimit interface {
+	IType
+	On() QNames
+	Rate() IRate
+}
+
+type IWithLimits interface {
+	// Returns Limit by name.
+	//
+	// Returns nil if not found.
+	Limit(QName) ILimit
+
+	// Enumerates all limits
+	//
+	// Limits are enumerated in alphabetical order by QName
+	Limits(func(ILimit))
+}
+
+type ILimitsBuilder interface {
+	// Adds new Limit type with specified name.
+	//
+	// # Panics:
+	//   - if name is empty or invalid,
+	//   - if type with the same name already exists,
+	//	 - if no rated objects names specified,
+	//	 - if rate is not found.
+	AddLimit(name QName, on []QName, rate QName, comment ...string)
+}
