9255169df5e2b374a91fcf2ced3a3d0dc9c2c6b1
Michael Saigachenko <m.saigachenko@dev.untill.com>
2024-05-03 15:47:05 +0200

    #1997 Add QueryContext Storage(#2011)

--- pkg/exttinygo/consts.go
+++ pkg/exttinygo/consts.go
@@ -16,6 +16,7 @@ const (
 	StorageRequestSubject = "sys.RequestSubject"
 	StorageResult         = "sys.Result"
 	StorageCommandContext = "sys.CommandContext"
+	StorageQueryContext   = "sys.QueryContext"
 
 	NullEntity = ""
 )
---
+++ pkg/state/impl_querycontext_storage_test.go
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2023-present unTill Pro, Ltd.
+ */
+
+package state
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/voedger/voedger/pkg/appdef"
+	"github.com/voedger/voedger/pkg/istructs"
+)
+
+func TestQueryContextStorage(t *testing.T) {
+	wsid := istructs.NullWSID
+	arg := istructs.NewNullObject()
+
+	wsidFunc := SimpleWSIDFunc(wsid)
+	argFunc := func() istructs.IObject { return arg }
+
+	s := ProvideQueryProcessorStateFactory()(context.Background(), nil, nil, wsidFunc, nil, nil, nil, argFunc)
+
+	kb, err := s.KeyBuilder(QueryContext, appdef.NullQName)
+	require.NoError(t, err)
+
+	v, err := s.MustExist(kb)
+	require.NoError(t, err)
+	require.Equal(t, int64(wsid), v.AsInt64(Field_Workspace))
+	require.NotNil(t, v.AsValue(Field_ArgumentObject))
+}
