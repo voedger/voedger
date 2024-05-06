47743ca035a5f6578bf206eb48cb9eebd8258395
Nikolay <mail2nnv@gmail.com>
2024-05-06 12:36:01 +0300

    linter

--- pkg/goutils/set/set.go
+++ pkg/goutils/set/set.go
@@ -40,10 +40,10 @@ func (s Set[V]) AsArray() (a []V) {
 			continue
 		}
 		l := bits.TrailingZeros64(b)
-		h := UintSize - bits.LeadingZeros64(b)
+		h := uintSize - bits.LeadingZeros64(b)
 		for v := l; v < h; v++ {
 			if b&(1<<v) != 0 {
-				a = append(a, V(i*UintSize+v))
+				a = append(a, V(i*uintSize+v))
 			}
 		}
 	}
@@ -66,7 +66,7 @@ func (s Set[V]) AsBytes() []byte {
 // Clears specified elements from set.
 func (s *Set[V]) Clear(values ...V) {
 	for _, v := range values {
-		s.bitmap[v/UintSize] &^= 1 << (v % UintSize)
+		s.bitmap[v/uintSize] &^= 1 << (v % uintSize)
 	}
 }
 
@@ -84,7 +84,7 @@ func (s Set[V]) Clone() Set[V] {
 
 // Returns is Set contains specified value.
 func (s Set[V]) Contains(v V) bool {
-	return s.bitmap[v/UintSize]&(1<<(v%UintSize)) != 0
+	return s.bitmap[v/uintSize]&(1<<(v%uintSize)) != 0
 }
 
 // Returns is Set contains all from specified values.
@@ -115,10 +115,10 @@ func (s Set[V]) Enumerate(visit func(V)) {
 			continue
 		}
 		l := bits.TrailingZeros64(b)
-		h := UintSize - bits.LeadingZeros64(b)
+		h := uintSize - bits.LeadingZeros64(b)
 		for v := l; v < h; v++ {
 			if b&(1<<v) != 0 {
-				visit(V(i*UintSize + v))
+				visit(V(i*uintSize + v))
 			}
 		}
 	}
@@ -131,8 +131,8 @@ func (s Set[V]) First() (V, bool) {
 		if b == 0 {
 			continue
 		}
-		if l := bits.TrailingZeros64(b); l < UintSize {
-			return V(i*UintSize + l), true
+		if l := bits.TrailingZeros64(b); l < uintSize {
+			return V(i*uintSize + l), true
 		}
 	}
 
@@ -151,7 +151,7 @@ func (s Set[V]) Len() int {
 // Sets specified values to Set.
 func (s *Set[V]) Set(values ...V) {
 	for _, v := range values {
-		s.bitmap[v/UintSize] |= 1 << (v % UintSize)
+		s.bitmap[v/uintSize] |= 1 << (v % uintSize)
 	}
 }
 
@@ -180,10 +180,10 @@ func (s Set[V]) String() string {
 			continue
 		}
 		l := bits.TrailingZeros64(b)
-		h := UintSize - bits.LeadingZeros64(b)
+		h := uintSize - bits.LeadingZeros64(b)
 		for v := l; v < h; v++ {
 			if b&(1<<v) != 0 {
-				ss = append(ss, say(V(i*UintSize+v)))
+				ss = append(ss, say(V(i*uintSize+v)))
 			}
 		}
 	}
@@ -191,4 +191,4 @@ func (s Set[V]) String() string {
 	return fmt.Sprintf("[%v]", strings.Join(ss, " "))
 }
 
-const UintSize = bits.UintSize
+const uintSize = bits.UintSize
