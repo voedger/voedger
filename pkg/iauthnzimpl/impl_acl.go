1dc0e9dd0eaee19b2f4385f2fbc65d8e6a237773
Denis Gribanov <d.gribanov@dev.untill.com>
2024-05-07 14:18:52 +0300

    misc

--- pkg/iauthnzimpl/impl_acl.go
+++ pkg/iauthnzimpl/impl_acl.go
@@ -84,6 +84,14 @@ var defaultACL = ACL{
 		policy: ACPolicy_Deny,
 	},
 	{
+		desc: "revoke insert or update on wdoc.air.LastNumbers from all",
+		pattern: PatternType{
+			opKindsPattern: []iauthnz.OperationKindType{iauthnz.OperationKind_INSERT, iauthnz.OperationKind_UPDATE},
+			qNamesPattern:  []appdef.QName{qNameWDocLastNumbers},
+		},
+		policy: ACPolicy_Deny,
+	},
+	{
 		desc: "update only is allowed for CDoc<$wsKind> for WorkspaceOwner",
 		pattern: PatternType{
 			qNamesPattern: []appdef.QName{
