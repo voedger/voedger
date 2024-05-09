77eb2e9e20d6d10422e83f6c3842f67fce21107a
Denis Griabanov <d.gribanov@dev.untill.com>
2024-05-09 12:23:31 +0300

    misc

--- pkg/state/impl_records_storage_test.go
+++ pkg/state/impl_records_storage_test.go
@@ -268,7 +268,8 @@ func TestRecordsStorage_Insert(t *testing.T) {
 		On("PutString", fieldName, value)
 	cud := &mockCUD{}
 	cud.On("Create").Return(rw)
-	s := ProvideCommandProcessorStateFactory()(context.Background(), nil, nil, SimpleWSIDFunc(istructs.NullWSID), nil, func() istructs.ICUD { return cud }, nil, nil, 1, nil, nil, nil)
+	s := ProvideCommandProcessorStateFactory()(context.Background(), nil, nil, SimpleWSIDFunc(istructs.NullWSID), nil,
+		func() istructs.ICUD { return cud }, nil, nil, 1, nil, nil, nil, nil)
 	kb, err := s.KeyBuilder(Record, testRecordQName1)
 	require.NoError(err)
 
@@ -290,7 +291,8 @@ func TestRecordsStorage_Update(t *testing.T) {
 	sv := &recordsValue{record: r}
 	cud := &mockCUD{}
 	cud.On("Update", mock.Anything).Return(rw)
-	s := ProvideCommandProcessorStateFactory()(context.Background(), nil, nil, SimpleWSIDFunc(istructs.NullWSID), nil, func() istructs.ICUD { return cud }, nil, nil, 1, nil, nil, nil)
+	s := ProvideCommandProcessorStateFactory()(context.Background(), nil, nil, SimpleWSIDFunc(istructs.NullWSID), nil,
+		func() istructs.ICUD { return cud }, nil, nil, 1, nil, nil, nil, nil)
 	kb, err := s.KeyBuilder(Record, testRecordQName1)
 	require.NoError(err)
 
@@ -365,7 +367,8 @@ func TestRecordsStorage_ValidateInWorkspaces_Writes(t *testing.T) {
 		On("Get", istructs.WSID(1), mock.Anything).Return(&nilValue{}, nil).
 		On("PutBatch", mock.Anything, mock.Anything).Return(nil)
 
-	s := ProvideCommandProcessorStateFactory()(context.Background(), appStructsFunc(mockedStructs), nil, SimpleWSIDFunc(istructs.WSID(1)), nil, nil, nil, nil, 10, nil, nil, nil)
+	s := ProvideCommandProcessorStateFactory()(context.Background(), appStructsFunc(mockedStructs), nil,
+		SimpleWSIDFunc(istructs.WSID(1)), nil, nil, nil, nil, 10, nil, nil, nil, nil)
 
 	wrongSingleton := appdef.NewQName("test", "RecordX")
 	wrongKb, err := s.KeyBuilder(Record, wrongSingleton)
