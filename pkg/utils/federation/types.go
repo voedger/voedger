6f059bb44d4ec7ad5392048e85df30ea31d2a54b
Denis Gribanov <d.gribanov@dev.untill.com>
2024-05-11 23:12:54 +0300

    misc

--- pkg/utils/federation/types.go
+++ pkg/utils/federation/types.go
@@ -8,26 +8,9 @@ package federation
 import (
 	"net/url"
 
-	"github.com/voedger/voedger/pkg/in10n"
 	"github.com/voedger/voedger/pkg/istructs"
-	"github.com/voedger/voedger/pkg/sys/blobber"
 	coreutils "github.com/voedger/voedger/pkg/utils"
 )
-
-type IFederation interface {
-	// POST(relativeURL string, body string, optFuncs ...ReqOptFunc) (*HTTPResponse, error)
-	// GET(relativeURL string, body string, optFuncs ...ReqOptFunc) (*HTTPResponse, error)
-	Func(relativeURL string, body string, optFuncs ...coreutils.ReqOptFunc) (*coreutils.FuncResponse, error)
-	UploadBLOB(appQName istructs.AppQName, wsid istructs.WSID, blobName string, blobMimeType string, blobContent []byte,
-		optFuncs ...coreutils.ReqOptFunc) (blobID istructs.RecordID, err error)
-	UploadBLOBs(appQName istructs.AppQName, wsid istructs.WSID, blobs []blobber.BLOB, optFuncs ...coreutils.ReqOptFunc) (blobIDs []istructs.RecordID, err error)
-	ReadBLOB(appQName istructs.AppQName, wsid istructs.WSID, blobID istructs.RecordID, optFuncs ...coreutils.ReqOptFunc) (*coreutils.HTTPResponse, error)
-	URLStr() string
-	Port() int
-	N10NUpdate(key in10n.ProjectionKey, val int64, optFuncs ...coreutils.ReqOptFunc) error
-	N10NSubscribe(projectionKey in10n.ProjectionKey) (offsetsChan OffsetsChan, unsubscribe func(), err error)
-	AdminFunc(relativeURL string, body string, optFuncs ...coreutils.ReqOptFunc) (*coreutils.FuncResponse, error)
-}
 
 type implIFederation struct {
 	httpClient      coreutils.IHTTPClient
