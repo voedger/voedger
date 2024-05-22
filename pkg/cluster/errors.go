e02c967c21c6149540c91bef51bb086c1a4bf36b
Denis Gribanov <d.gribanov@dev.untill.com>
2024-05-22 16:29:52 +0300

    misc

--- pkg/cluster/errors.go
+++ pkg/cluster/errors.go
@@ -10,6 +10,5 @@ import "errors"
 var (
 	ErrNumPartitionsChanged    = errors.New("num partitions changed")
 	ErrNumAppWorkspacesChanged = errors.New("num application workspaces changed")
-	errWrongWhere              = errors.New("'where sys.ID = <id>' only is supported")
-	errWrongWhereForView = errors.New("'where viewField1 = val1 [and viewField2 = val2 ...]' condition is only supported")
+	errWrongWhereForView       = errors.New("'where viewField1 = val1 [and viewField2 = val2 ...]' condition is only supported")
 )
