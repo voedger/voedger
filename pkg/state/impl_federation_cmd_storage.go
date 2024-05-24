1727b6efcc1acd8cf7d5554663abc308eab09330
Michael Saigachenko <m.saigachenko@dev.untill.com>
2024-05-24 14:07:05 +0300

    #2122 Integration test for the FederationCommand storge(#2175)

--- pkg/state/impl_federation_cmd_storage.go
+++ pkg/state/impl_federation_cmd_storage.go
@@ -86,17 +86,25 @@ func (s *federationCommandStorage) Get(key istructs.IStateKeyBuilder) (istructs.
 
 	appOwnerAndName := owner + istructs.AppQNameQualifierChar + appname
 
-	relativeUrl := fmt.Sprintf("api/%s/%d/%s", appOwnerAndName, wsid, command)
+	relativeUrl := fmt.Sprintf("api/%s/%d/c.%s", appOwnerAndName, wsid, command)
 
 	var resStatus int
 	var resBody string
 	var newIDs map[string]int64
 	var err error
+	var result map[string]interface{}
 
 	if s.emulation != nil {
 		resStatus, newIDs, resBody, err = s.emulation(owner, appname, wsid, command, body)
 		if err != nil {
 			return nil, err
+		}
+		result = map[string]interface{}{}
+		if resBody != "" {
+			err = json.Unmarshal([]byte(resBody), &result)
+			if err != nil {
+				return nil, err
+			}
 		}
 	} else {
 
@@ -115,15 +123,17 @@ func (s *federationCommandStorage) Get(key istructs.IStateKeyBuilder) (istructs.
 		if err != nil {
 			return nil, err
 		}
-		resBody = resp.Body
+
 		newIDs = resp.NewIDs
 		resStatus = resp.HTTPResp.StatusCode
-	}
 
-	result := map[string]interface{}{}
-	err = json.Unmarshal([]byte(resBody), &result)
-	if err != nil {
-		return nil, err
+		res := map[string]interface{}{}
+		err = json.Unmarshal([]byte(resp.Body), &res)
+		if err != nil {
+			return nil, err
+		}
+		result = res["Result"].(map[string]interface{})
+
 	}
 
 	return &fcCmdValue{
