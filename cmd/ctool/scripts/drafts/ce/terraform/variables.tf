547fb318fd4d8042bf5a790713dc947b0f8d6f96
AlekseiPonomarev <paa@sigma-soft.spb.ru>
2024-06-04 16:27:51 +0300

    misc

--- cmd/ctool/scripts/drafts/ce/terraform/variables.tf
+++ cmd/ctool/scripts/drafts/ce/terraform/variables.tf
@@ -1,4 +1,6 @@
-variable "ssh_private_key" {
-  description = "Path to the private key file for SSH."
-  type        = string
-}
+variable "ssh_private_key" {}
+variable "gh_token" {}
+variable "git_commit_id" {}
+variable "git_repo_url" {}
+variable "ssh_port" {}
+variable "issue_number" {}
