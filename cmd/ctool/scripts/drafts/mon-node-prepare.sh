1b460e7a6410a7fcd6764650435b137be07a847f
AlekseiPonomarev <paa@sigma-soft.spb.ru>
2024-07-11 12:41:19 +0300

    misc

--- cmd/ctool/scripts/drafts/mon-node-prepare.sh
+++ cmd/ctool/scripts/drafts/mon-node-prepare.sh
@@ -115,7 +115,7 @@ EOF
 
   cat ./prometheus/web.yml | utils_ssh "$SSH_USER@$1" 'cat > ~/prometheus/web.yml'
   if utils_ssh "SSH_USER@$1" 'if [ ! -f ~/prometheus/alert.rules ]; then exit 0; else exit 1; fi'; then
-      cat ./prometheus/alert.rules | utils_ssh "$SSH_USER@$1" 'cat > ~/prometheus/alert.rules'
+      cat ./prometheus/alert.rules | utils_ssh "$SSH_USER@$1" 'cat > ~/prometheus/alert.rules';
   fi
   cat ./alertmanager/config.yml | utils_ssh "$SSH_USER@$1" 'cat > ~/alertmanager/config.yml'
 
