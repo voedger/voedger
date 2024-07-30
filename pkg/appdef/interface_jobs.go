b92d959243c6bb6d75ade031f229dc4752b7231c
Nikolay <mail2nnv@gmail.com>
2024-07-30 15:37:07 +0300

    + jobs interface

---
+++ pkg/appdef/interface_jobs.go
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2021-present Sigma-Soft, Ltd.
+ * @author: Nikolay Nikitin
+ */
+
+package appdef
+
+// Job is a extension that executes by schedule.
+type IJob interface {
+	IExtension
+
+	// Schedule to trigger projector as cron expression.
+	CronSchedule() string
+}
+
+type IJobBuilder interface {
+	IExtensionBuilder
+
+	// Set schedule to trigger projector as cron expression.
+	SetCronSchedule(string) IJobBuilder
+}
+
+type IWithJobs interface {
+	// Return job by name.
+	//
+	// Returns nil if not found.
+	Job(QName) IJob
+
+	// Enumerates all jobs.
+	//
+	// Jobs are enumerated in alphabetical order by QName.
+	Jobs(func(IJob))
+}
+
+type IJobsBuilder interface {
+	// Add new job
+	//
+	// # Panics:
+	//   - if name is empty (appdef.NullQName),
+	//   - if name is invalid,
+	//   - if type with name already exists.
+	AddJob(QName) IJobBuilder
+}
