0f631977cb1d2a7868f424cd5faac7b40b9eff9d
Denis Gribanov <instance6@gmail.com>
2024-09-04 15:34:16 +0300

    #2571 fix timers firing(#2572)

--- pkg/utils/time_test.go
+++ pkg/utils/time_test.go
@@ -22,10 +22,10 @@ func TestTime_BasicUsage(t *testing.T) {
 	})
 
 	t.Run("timer", func(t *testing.T) {
-		timer := tm.NewTimer(100 * time.Millisecond)
+		timer := tm.NewTimerChan(100 * time.Millisecond)
 		start := tm.Now()
 		firingInstant := <-timer
-		require.Less(firingInstant.UnixMilli()-start.UnixMilli(), int64(130)) // 30 msecs lag for slow PCs
+		require.Less(firingInstant.UnixMilli()-start.UnixMilli(), int64(100+30)) // 30 msecs lag for slow PCs
 	})
 }
 
@@ -46,8 +46,8 @@ func TestMockTime(t *testing.T) {
 	})
 
 	t.Run("timer", func(t *testing.T) {
-		timer1 := MockTime.NewTimer(123 * time.Hour)
-		timer2 := MockTime.NewTimer(125 * time.Hour)
+		timer1 := MockTime.NewTimerChan(123 * time.Hour)
+		timer2 := MockTime.NewTimerChan(125 * time.Hour)
 		MockTime.Add(122 * time.Hour)
 		select {
 		case <-timer1:
@@ -57,24 +57,92 @@ func TestMockTime(t *testing.T) {
 		default:
 		}
 
-		MockTime.Add(2 * time.Hour) // +124 hours
+		MockTime.Add(1 * time.Hour) // +123 hours
 
+		var firingInstant time.Time
 		select {
-		case <-timer1:
+		case firingInstant = <-timer1:
 		case <-timer2:
 			t.Fatal(3)
 		default:
 			t.Fatal(4)
 		}
 
-		MockTime.Add(time.Hour) // +125 hours
+		require.Equal(MockTime.Now(), firingInstant)
+
+		// cross over timer2
+		MockTime.Add(3 * time.Hour) // +126 hours
 
 		select {
 		case <-timer1:
 			t.Fatal(5)
-		case <-timer2:
+		case firingInstant = <-timer2:
 		default:
 			t.Fatal(6)
+		}
+
+		require.Equal(MockTime.Now(), firingInstant)
+	})
+}
+
+func TestMockTimer(t *testing.T) {
+	timer := MockTime.NewTimerChan(time.Hour)
+	MockTime.Add(59 * time.Minute)
+	select {
+	case <-timer:
+		t.Fatal(1)
+	default:
+	}
+	MockTime.Add(time.Minute)
+	firingInstant := <-timer
+	require.Equal(t, firingInstant, MockTime.Now())
+}
+
+func BenchmarkMockTimers(b *testing.B) {
+	b.Run("create only", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			_ = MockTime.NewTimerChan(time.Hour)
+		}
+	})
+
+	b.Run("create and fire", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			_ = MockTime.NewTimerChan(time.Hour)
+			MockTime.Add(time.Hour)
+		}
+	})
+
+	b.Run("create, fire and read", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			timerChan := MockTime.NewTimerChan(time.Hour)
+			MockTime.Add(time.Hour)
+			<-timerChan
+		}
+	})
+}
+
+func BenchmarkRealTimers(b *testing.B) {
+	b.Run("create only", func(b *testing.B) {
+		t := NewITime()
+		for i := 0; i < b.N; i++ {
+			_ = t.NewTimerChan(time.Hour)
+		}
+	})
+
+	b.Run("create and fire", func(b *testing.B) {
+		t := NewITime()
+		for i := 0; i < b.N; i++ {
+			_ = t.NewTimerChan(time.Millisecond)
+			time.Sleep(2 * time.Millisecond)
+		}
+	})
+
+	b.Run("create, fire and read", func(b *testing.B) {
+		t := NewITime()
+		for i := 0; i < b.N; i++ {
+			timerChan := t.NewTimerChan(time.Millisecond)
+			time.Sleep(2 * time.Millisecond)
+			<-timerChan
 		}
 	})
 }
