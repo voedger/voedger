04cab3b58e4ef551ed1913ca8c7e7975125efb72
michael-say <smmbox@gmail.com>
2024-09-07 13:40:32 +0200

    StateStorageHttp: add verbose logging

--- pkg/sys/storages/impl_http_storage.go
+++ pkg/sys/storages/impl_http_storage.go
@@ -11,13 +11,17 @@ import (
 	"io"
 	"net/http"
 	"strings"
+	"sync/atomic"
 	"time"
 
 	"github.com/voedger/voedger/pkg/appdef"
+	"github.com/voedger/voedger/pkg/goutils/logger"
 	"github.com/voedger/voedger/pkg/istructs"
 	"github.com/voedger/voedger/pkg/state"
 	"github.com/voedger/voedger/pkg/sys"
 )
+
+var requestNumber int64
 
 type httpStorage struct {
 	customClient state.IHttpClient
@@ -159,7 +163,11 @@ func (s *httpStorage) Read(key istructs.IStateKeyBuilder, callback istructs.Valu
 	for k, v := range kb.headers {
 		req.Header.Add(k, v)
 	}
-
+	var reqNumber int64
+	if logger.IsVerbose() {
+		reqNumber = atomic.AddInt64(&requestNumber, 1)
+		logger.Verbose("req ", reqNumber, ": ", method, " ", kb.url, " body: ", string(kb.body))
+	}
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return err
@@ -169,6 +177,10 @@ func (s *httpStorage) Read(key istructs.IStateKeyBuilder, callback istructs.Valu
 	bb, err := io.ReadAll(res.Body)
 	if err != nil {
 		return err
+	}
+
+	if logger.IsVerbose() {
+		logger.Verbose("resp ", reqNumber, ": ", res.StatusCode, " body: ", string(kb.body))
 	}
 
 	return callback(nil, &httpValue{
