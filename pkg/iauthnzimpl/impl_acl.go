9c4ae14d18fb55fe3060a0309aafb7fd552a9a88
Michael <m.saigachenko@dev.untill.com>
2024-09-15 12:12:48 +0200

    acl for fiscalcloud

--- pkg/iauthnzimpl/impl_acl.go
+++ pkg/iauthnzimpl/impl_acl.go
@@ -60,13 +60,14 @@ var defaultACL = ACL{
 		policy: ACPolicy_Allow,
 	},
 	{
-		desc: "allowed to Guest",
+		desc: "allowed to sys.Guest login, i.e. without principal token at all",
 		pattern: PatternType{
-			principalsPattern: [][]iauthnz.Principal{{{Kind: iauthnz.PrincipalKind_User, QName: qNameSysGuest}}},
+			principalsPattern: [][]iauthnz.Principal{{{Kind: iauthnz.PrincipalKind_User, WSID: istructs.GuestWSID}}},
 			qNamesPattern: []appdef.QName{
 				qNameCmdProvideCertificatePart, qNameCmdProvideCertificate, qNameQryGetCustomerStatus,
 				qNameCmdFiscalizeDocument, qNameQryFiscalizationResultStatus, qNameCmdCreateExport, qNameQryExportStatus},
 		},
+		policy: ACPolicy_Allow,
 	},
 	{
 		desc: "everything is allowed to WorkspaceOwner",
