2b6f2ae54488f2d8dbdfb2cbe4816c20ef33cbf9
Denis Gribanov <instance6@gmail.com>
2024-11-29 18:28:20 +0300

    #2901 vpm orm: fix wrong orm generation Resolves #2895 (#2918)

--- cmd/vpm/main_test.go
+++ cmd/vpm/main_test.go
@@ -318,7 +318,6 @@ func TestOrmBasicUsage(t *testing.T) {
 }
 
 func TestBuildExample2(t *testing.T) {
-	t.Skip("wait for https://github.com/voedger/voedger/issues/2901")
 	if testing.Short() {
 		t.Skip()
 	}
@@ -515,7 +514,6 @@ func TestBuildBasicUsage(t *testing.T) {
 }
 
 func TestGenOrmTestItAndBuildApp(t *testing.T) {
-	t.Skip("wait for https://github.com/voedger/voedger/issues/2901")
 	if testing.Short() {
 		t.Skip()
 	}
@@ -537,23 +535,23 @@ func TestGenOrmTestItAndBuildApp(t *testing.T) {
 	require.NoError(err)
 
 	// test runs in the temp directory
-	dir := filepath.Join(tempDir, "air")
+	tempAirDir := filepath.Join(tempDir, "air")
 
 	// go up to the root of the project.
 	localVoedgerDir := filepath.Join(wd, "..", "..")
 
 	// replace the voedger package with the local one in the go.mod file
 	// we use an absolute path so that we don't depend on where the test is running.
-	err = new(exec.PipedExec).Command("go", "mod", "edit", "-replace", "github.com/voedger/voedger="+localVoedgerDir).WorkingDir(dir).Run(os.Stdout, os.Stderr)
+	err = new(exec.PipedExec).Command("go", "mod", "edit", "-replace", "github.com/voedger/voedger="+localVoedgerDir).WorkingDir(tempAirDir).Run(os.Stdout, os.Stderr)
 	require.NoError(err)
 
-	err = execRootCmd([]string{"vpm", "orm", "-C", dir}, "1.0.0")
+	err = execRootCmd([]string{"vpm", "orm", "-C", tempAirDir}, "1.0.0")
 	require.NoError(err)
 
-	err = new(exec.PipedExec).Command("go", "test", filepath.Join(dir, "wasm")).WorkingDir(dir).Run(os.Stdout, os.Stderr)
+	err = new(exec.PipedExec).Command("go", "test", filepath.Join(tempAirDir, "wasm")).WorkingDir(tempAirDir).Run(os.Stdout, os.Stderr)
 	require.NoError(err)
 
-	err = execRootCmd([]string{"vpm", "build", "-C", dir}, "1.0.0")
+	err = execRootCmd([]string{"vpm", "build", "-C", tempAirDir}, "1.0.0")
 	require.NoError(err)
 }
 
