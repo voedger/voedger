fdba03a4776b2ed43c66551dc36867969b7e2a9b
Denis Gribanov <instance6@gmail.com>
2025-01-25 17:59:47 +0300

    #3188 pseudoWSID -> appWSID: by router -> by vmm(#3190)

--- pkg/bus/types.go
+++ pkg/bus/types.go
@@ -14,15 +14,14 @@ import (
 )
 
 type Request struct {
-	Method      string
-	WSID        istructs.WSID
-	PartitionID istructs.PartitionID
-	Header      map[string][]string `json:",omitempty"`
-	Resource    string
-	Query       map[string][]string `json:",omitempty"`
-	Body        []byte              `json:",omitempty"`
-	AppQName    string
-	Host        string // used by authenticator to emit Host principal
+	Method   string
+	WSID     istructs.WSID // as it came in the request, could be pseudo
+	Header   map[string][]string
+	Resource string
+	Query    map[string][]string
+	Body     []byte
+	AppQName string
+	Host     string // used by authenticator to emit Host principal
 }
 
 type ResponseMeta struct {
