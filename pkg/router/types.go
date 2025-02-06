3042320f950127370aa2000e05093ab55a4542b2
Denis Gribanov <instance6@gmail.com>
2025-02-06 15:46:15 +0300

    #3232 pseudoWSID->appWSID conversion VVM -> back to router (rollback #3188)(#3233)

--- pkg/router/types.go
+++ pkg/router/types.go
@@ -15,8 +15,10 @@ import (
 	"github.com/gorilla/mux"
 	"golang.org/x/crypto/acme/autocert"
 
+	"github.com/voedger/voedger/pkg/appdef"
 	"github.com/voedger/voedger/pkg/bus"
 	"github.com/voedger/voedger/pkg/in10n"
+	"github.com/voedger/voedger/pkg/istructs"
 	blobprocessor "github.com/voedger/voedger/pkg/processors/blobber"
 )
 
@@ -35,14 +37,14 @@ type RouterParams struct {
 
 type httpService struct {
 	RouterParams
-	listenAddress string
-	router        *mux.Router
-	server        *http.Server
-	listener      net.Listener
-	n10n          in10n.IN10nBroker
-	blobWG        sync.WaitGroup
-	requestSender bus.IRequestSender
-
+	listenAddress      string
+	router             *mux.Router
+	server             *http.Server
+	listener           net.Listener
+	n10n               in10n.IN10nBroker
+	blobWG             sync.WaitGroup
+	requestSender      bus.IRequestSender
+	numsAppsWorkspaces map[appdef.AppQName]istructs.NumAppWorkspaces
 	name               string
 	listeningPort      atomic.Uint32
 	blobRequestHandler blobprocessor.IRequestHandler
