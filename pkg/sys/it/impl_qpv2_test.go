fa8c7e3db6c3fe23b73fce4aa4beedd3e643046f
michael-say <smmbox@gmail.com>
2025-02-08 12:55:53 +0100

    Enhance TestQueryProcessor_V2 by adding projection update and query execution

--- pkg/sys/it/impl_qpv2_test.go
+++ pkg/sys/it/impl_qpv2_test.go
@@ -8,13 +8,36 @@ package sys_it
 import (
 	"testing"
 
+	"github.com/stretchr/testify/require"
+	"github.com/voedger/voedger/pkg/appdef"
+	"github.com/voedger/voedger/pkg/in10n"
+	"github.com/voedger/voedger/pkg/istructs"
 	"github.com/voedger/voedger/pkg/vit"
 )
 
 func TestQueryProcessor_V2(t *testing.T) {
-	t.Skip()
+	//t.Skip()
+	require := require.New(t)
 	vit := vit.NewVIT(t, &vit.SharedConfig_App1)
 	defer vit.TearDown()
 
-	vit.IFederation.Func("api/v2/users/test1/apps/app1/workspaces/123/docs/app1pkg.category", "{}")
+	ws := vit.WS(istructs.AppQName_test1_app1, "test_ws")
+	testProjectionKey := in10n.ProjectionKey{
+		App:        istructs.AppQName_test1_app1,
+		Projection: appdef.NewQName("app1pkg", "CategoryIdx"),
+		WS:         ws.WSID,
+	}
+
+	offsetsChan, unsubscribe := vit.SubscribeForN10nUnsubscribe(testProjectionKey)
+
+	// force projection update
+	body := `{"cuds":[{"fields":{"sys.ID":1,"sys.QName":"app1pkg.category","name":"Awesome food"}}]}`
+	resultOffsetOfCUD := vit.PostWS(ws, "c.sys.CUD", body).CurrentWLogOffset
+	require.EqualValues(resultOffsetOfCUD, <-offsetsChan)
+	unsubscribe()
+
+	// execute query
+	resp, err := vit.IFederation.Func("api/v2/users/test1/apps/app1/workspaces/123/views/app1pkg.CategoryIdx", "{}")
+	require.NoError(err)
+	require.Nil(resp.SysError)
 }
