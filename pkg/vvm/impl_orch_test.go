e81fd33926adedebd9eb832d9940e3b8acaac578
Denis Gribanov <instance6@gmail.com>
2025-02-28 18:29:51 +0300

    #3390 make TestAutomaticShutdownOnLeadershipLoss() be the single in the package(#3391)

--- pkg/vvm/impl_orch_test.go
+++ pkg/vvm/impl_orch_test.go
@@ -10,32 +10,22 @@ import (
 	"net"
 	"sync"
 	"testing"
-	"testing/fstest"
 	"time"
 
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/require"
 
-	"github.com/voedger/voedger/pkg/appparts"
 	"github.com/voedger/voedger/pkg/coreutils"
 	"github.com/voedger/voedger/pkg/coreutils/utils"
-	"github.com/voedger/voedger/pkg/extensionpoints"
 	"github.com/voedger/voedger/pkg/istorage"
 	"github.com/voedger/voedger/pkg/istructs"
-	"github.com/voedger/voedger/pkg/istructsmem"
-	"github.com/voedger/voedger/pkg/parser"
-	"github.com/voedger/voedger/pkg/sys/smtp"
-	"github.com/voedger/voedger/pkg/sys/sysprovide"
-	builtinapps "github.com/voedger/voedger/pkg/vvm/builtin"
-	"github.com/voedger/voedger/pkg/vvm/builtin/clusterapp"
-	"github.com/voedger/voedger/pkg/vvm/builtin/registryapp"
 )
 
 func TestBasic(t *testing.T) {
 	t.Run("VVMStartAndStop", func(t *testing.T) {
 		r := require.New(t)
 
-		vvmCfg1 := getTestVVMCfg(net.IPv4(192, 168, 0, 1))
+		vvmCfg1 := GetTestVVMCfg(net.IPv4(192, 168, 0, 1))
 		vvm1, err := Provide(vvmCfg1)
 		r.NoError(err)
 
@@ -51,7 +41,7 @@ func TestBasic(t *testing.T) {
 		r := require.New(t)
 
 		iTime := coreutils.MockTime
-		vvmCfg1 := getTestVVMCfg(net.IPv4(192, 168, 0, 1))
+		vvmCfg1 := GetTestVVMCfg(net.IPv4(192, 168, 0, 1))
 
 		// make so that VVM launch on vvmCfg1 will store the resulting storage in sharedStorageFactory
 		suffix := t.Name() + uuid.NewString()
@@ -75,7 +65,7 @@ func TestBasic(t *testing.T) {
 		go func() {
 			defer wg.Done()
 
-			vvmCfg2 := getTestVVMCfg(net.IPv4(192, 168, 0, 2))
+			vvmCfg2 := GetTestVVMCfg(net.IPv4(192, 168, 0, 2))
 
 			// set vvmCfg2 storage factory to the one from vvm1
 			vvmCfg2.StorageFactory = func() (provider istorage.IAppStorageFactory, err error) {
@@ -109,7 +99,7 @@ func TestBasic(t *testing.T) {
 func TestCancelLeadershipOnManualShutdown(t *testing.T) {
 	r := require.New(t)
 
-	vvmCfg := getTestVVMCfg(net.IPv4(192, 168, 0, 1))
+	vvmCfg := GetTestVVMCfg(net.IPv4(192, 168, 0, 1))
 	vvm, err := Provide(vvmCfg)
 	r.NoError(err)
 
@@ -145,7 +135,7 @@ func TestCancelLeadershipOnManualShutdown(t *testing.T) {
 func TestServicePipelineStartFailure(t *testing.T) {
 	require := require.New(t)
 
-	vvmCfg := getTestVVMCfg(net.IPv4(192, 168, 0, 1))
+	vvmCfg := GetTestVVMCfg(net.IPv4(192, 168, 0, 1))
 	vvmCfg.VVMPort = -1
 	vvm, err := Provide(vvmCfg)
 	require.NoError(err)
@@ -161,7 +151,7 @@ func TestServicePipelineStartFailure(t *testing.T) {
 func TestWrongLaunchAndShutdownUsage(t *testing.T) {
 	require := require.New(t)
 
-	vvmCfg := getTestVVMCfg(net.IPv4(192, 168, 0, 1))
+	vvmCfg := GetTestVVMCfg(net.IPv4(192, 168, 0, 1))
 	vvm, err := Provide(vvmCfg)
 	require.NoError(err)
 
@@ -174,85 +164,4 @@ func TestWrongLaunchAndShutdownUsage(t *testing.T) {
 		defer vvm.Shutdown()
 		require.Panics(func() { vvm.Launch(DefaultLeadershipDurationSeconds, DefaultLeadershipAcquisitionDuration) })
 	})
-}
-
-// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-// note: this test must be the last in the entire PACKAGE, not the just last in the file
-// otherwise killerRoutine will kill the entire test process if the rest of test will last longer than 5 seconds
-// [~server.design.orch/VVM.test.Shutdown~impl]
-func TestAutomaticShutdownOnLeadershipLoss(t *testing.T) {
-	r := require.New(t)
-
-	vvmCfg := getTestVVMCfg(net.IPv4(192, 168, 0, 1))
-	vvm1, err := Provide(vvmCfg)
-	r.NoError(err)
-
-	// Launch VVM1
-	problemCtx := vvm1.Launch(DefaultLeadershipDurationSeconds, DefaultLeadershipAcquisitionDuration)
-	r.NoError(problemCtx.Err())
-
-	// Simulate leadership loss
-	pKey := make([]byte, utils.Uint32Size)
-	cCols := make([]byte, utils.Uint32Size)
-	binary.BigEndian.PutUint32(pKey, 1)
-	binary.BigEndian.PutUint32(cCols, uint32(1))
-
-	vvmAppTTLStorage, err := vvm1.APIs.IAppStorageProvider.AppStorage(istructs.AppQName_sys_vvm)
-	require.NoError(t, err)
-	ok, err := vvmAppTTLStorage.CompareAndSwap(
-		pKey,
-		cCols,
-		[]byte(vvmCfg.IP.String()),
-		[]byte("another_value"),
-		50,
-	)
-	r.NoError(err)
-	r.True(ok)
-
-	// Bump mock time
-	coreutils.MockTime.Sleep(time.Duration(DefaultLeadershipDurationSeconds) * time.Second)
-
-	// Check problem context
-	<-problemCtx.Done()
-	r.ErrorIs(vvm1.Shutdown(), ErrLeadershipLost)
-}
-
-func getTestVVMCfg(ip net.IP) *VVMConfig {
-	vvmCfg := NewVVMDefaultConfig()
-	vvmCfg.VVMPort = 0
-	vvmCfg.IP = ip
-	vvmCfg.MetricsServicePort = 0
-	vvmCfg.Time = coreutils.MockTime
-	vvmCfg.VVMAppsBuilder.Add(istructs.AppQName_test1_app1, func(apis builtinapps.APIs, cfg *istructsmem.AppConfigType, ep extensionpoints.IExtensionPoint) builtinapps.Def {
-		sysPackageFS := sysprovide.Provide(cfg)
-		return builtinapps.Def{
-			AppDeploymentDescriptor: appparts.AppDeploymentDescriptor{
-				NumParts:         10,
-				EnginePoolSize:   appparts.PoolSize(10, 10, 20, 10),
-				NumAppWorkspaces: istructs.DefaultNumAppWorkspaces,
-			},
-			AppQName: istructs.AppQName_test1_app1,
-			Packages: []parser.PackageFS{{
-				Path: "github.com/voedger/voedger/pkg/app1",
-				FS: fstest.MapFS{
-					"app.vsql": &fstest.MapFile{
-						Data: []byte(`
-							APPLICATION app1();
-
-							ALTERABLE WORKSPACE test_wsWS (
-
-								DESCRIPTOR test_ws (
-									IntFld int32 NOT NULL,
-									StrFld varchar(1024)
-								);
-							);`),
-					},
-				},
-			}, sysPackageFS},
-		}
-	})
-	vvmCfg.VVMAppsBuilder.Add(istructs.AppQName_sys_registry, registryapp.Provide(smtp.Cfg{}, 10))
-	vvmCfg.VVMAppsBuilder.Add(istructs.AppQName_sys_cluster, clusterapp.Provide())
-
-	return &vvmCfg
 }
