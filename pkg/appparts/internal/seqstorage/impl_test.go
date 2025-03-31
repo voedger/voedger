d3da9ff61e504fbc12456ac9209187ac7fb358ac
Denis Gribanov <d.gribanov@dev.untill.com>
2025-03-31 15:24:10 +0300

    misc

--- pkg/appparts/internal/seqstorage/impl_test.go
+++ pkg/appparts/internal/seqstorage/impl_test.go
@@ -12,6 +12,9 @@ import (
 	"github.com/voedger/voedger/pkg/appdef"
 	"github.com/voedger/voedger/pkg/appdef/builder"
 	"github.com/voedger/voedger/pkg/coreutils"
+	"github.com/voedger/voedger/pkg/isequencer"
+	"github.com/voedger/voedger/pkg/istorage/mem"
+	"github.com/voedger/voedger/pkg/istorage/provider"
 	"github.com/voedger/voedger/pkg/istructs"
 	"github.com/voedger/voedger/pkg/vvm/storage"
 )
@@ -26,6 +29,48 @@ func TestBasicUsage(t *testing.T) {
 	ws.AddCDoc(testCDocQName).AddField("IntFld", appdef.DataKind_int32, false)
 	appDef, err := appDefBuilder.Build()
 	require.NoError(err)
-	seqSysVVMStorage := storage.NewSeqStorage()
-	New(istructs.PartitionID(1), mockEvents, appDef, )
+	appStorageProvider := provider.Provide(mem.Provide(coreutils.MockTime))
+	appStorage, err := appStorageProvider.AppStorage(istructs.AppQName_sys_vvm)
+	require.NoError(err)
+	seqSysVVMStorage := storage.NewSeqStorage(appStorage)
+	seqStorage := New(istructs.PartitionID(1), mockEvents, appDef, seqSysVVMStorage)
+
+	err = seqStorage.WriteValues([]isequencer.SeqValue{
+		{Key: isequencer.NumberKey{WSID: 1, SeqID: 1}, Value: 1},
+		{Key: isequencer.NumberKey{WSID: 1, SeqID: 1}, Value: 2},
+		{Key: isequencer.NumberKey{WSID: 2, SeqID: 1}, Value: 3},
+		{Key: isequencer.NumberKey{WSID: 2, SeqID: 2}, Value: 1},
+	})
+	require.NoError(err)
+
+	err = seqStorage.WriteNextPLogOffset(isequencer.PLogOffset(5))
+	require.NoError(err)
+
+	numbers, err := seqStorage.ReadNumbers(1, []isequencer.SeqID{1})
+	require.NoError(err)
+	require.Equal()
+
+
+	// t.Run("read empty", func(t *testing.T) {
+	// 	t.Run("ReadNumbers", func(t *testing.T) {
+	// 		numbers, err := seqStorage.ReadNumbers(1, nil)
+	// 		require.NoError(err)
+	// 		require.Empty(numbers)
+	// 	})
+
+	// 	t.Run("ReadPLogOffset", func(t *testing.T) {
+	// 		offset, err := seqStorage.ReadNextPLogOffset()
+	// 		require.NoError(err)
+	// 		require.Equal(isequencer.PLogOffset(0), offset)
+	// 	})
+
+	// 	t.Run("Actualize", func(t *testing.T) {
+	// 		ctx := context.Background()
+	// 		err := seqStorage.ActualizeSequencesFromPLog(ctx, isequencer.PLogOffset(1), func(batch []isequencer.SeqValue, offset isequencer.PLogOffset) error {
+	// 			t.Fail()
+	// 			return nil
+	// 		})
+	// 		require.NoError(err)
+	// 	})
+	// })
 }
