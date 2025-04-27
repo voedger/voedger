ebe52b70346d5e1bf7cdd2d1eb9fdeacc741bfb0
Denis Gribanov <instance6@gmail.com>
2025-04-27 17:45:29 +0300

    #3615 minor refactor(#3616)

--- pkg/itokensjwt/provide.go
+++ pkg/itokensjwt/provide.go
@@ -13,8 +13,6 @@ import (
 
 // ProvideITokens implementation by provided interface
 // To receive implementation you must provide Secret Key. Min length - 64 byte, panic otherwise
-// panics on try to init again with different ITime implementation to avoid cases when we have >1 VIT that uses different times
-// jwt.TimeFunc is single point of the time per the whole process so all tests must share exactly one implementation of ITime
 func ProvideITokens(secretKey SecretKeyType, time coreutils.ITime) (tokenImpl itokens.ITokens) {
 	return NewJWTSigner(secretKey, time)
 }
