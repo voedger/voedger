b2ff7f0eb6f083c91df445a4600a03fbfde7eeca
Michael <m.saigachenko@dev.untill.com>
2025-05-10 17:40:40 +0200

    Rename query parameter from "arg" to "args" across multiple files for consistency

--- pkg/processors/query2/impl_auth_login_handler.go
+++ pkg/processors/query2/impl_auth_login_handler.go
@@ -34,7 +34,7 @@ func authLoginHandler() apiPathHandler {
 
 			pseudoWSID := coreutils.GetPseudoWSID(istructs.NullWSID, login, istructs.CurrentClusterID())
 
-			url := fmt.Sprintf(`api/v2/apps/%s/%s/workspaces/%d/queries/registry.IssuePrincipalToken?arg=%s`,
+			url := fmt.Sprintf(`api/v2/apps/%s/%s/workspaces/%d/queries/registry.IssuePrincipalToken?args=%s`,
 				istructs.SysOwner, istructs.AppQName_sys_registry.Name(), pseudoWSID,
 				url.QueryEscape(fmt.Sprintf(`{"Login":"%s", "Password":"%s", "AppName": "%s"}`, login, password, qw.msg.AppQName())))
 			resp, err := qw.federation.QueryNoRetry(url)
