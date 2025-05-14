1b22b80316ee2f48b613ac93cd2ffa12b6006ce8
Nikolay <mail2nnv@gmail.com>
2025-05-14 12:41:34 +0300

    small integer tech debt

--- pkg/istructsmem/internal/dynobuf/utils.go
+++ pkg/istructsmem/internal/dynobuf/utils.go
@@ -26,8 +26,6 @@ func NewFieldsScheme(name string, fields appdef.IWithFields) *dynobuffers.Scheme
 				switch f.DataKind() {
 				case appdef.DataKind_int8: // #3435 [~server.vsql.smallints/cmp.istructsmem~impl]
 					db.AddField(f.Name(), ft, false)
-				case appdef.DataKind_int16: // #3435 [~server.vsql.smallints/cmp.istructsmem~impl]
-					db.AddArray(f.Name(), ft, false) // two fixed bytes LittleEndian
 				case appdef.DataKind_QName:
 					db.AddArray(f.Name(), ft, false) // two fixed bytes LittleEndian
 				default: // bytes, record, event
