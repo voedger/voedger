8c94d1d04d517682b3c910ddd5c05f4dff8ccb97
Denis Gribanov <d.gribanov@dev.untill.com>
2025-06-05 11:18:00 +0300

    misc

--- pkg/sys/it/impl_n10n_test.go
+++ pkg/sys/it/impl_n10n_test.go
@@ -127,3 +127,33 @@ func TestSynthetic(t *testing.T) {
 
 	<-done // wait for event read and offsestChan close
 }
+
+func TestChannelExpiration(t *testing.T) {
+	require := require.New(t)
+	vit := it.NewVIT(t, &it.SharedConfig_App1)
+	defer vit.TearDown()
+
+	ws := vit.WS(istructs.AppQName_test1_app1, "test_ws")
+	testProjectionKey := in10n.ProjectionKey{
+		App:        istructs.AppQName_test1_app1,
+		Projection: appdef.NewQName("paa", "price"),
+		WS:         ws.WSID,
+	}
+
+	offsetsChan, unsubscribe, err := vit.N10NSubscribe(testProjectionKey)
+	require.NoError(err)
+
+	// expire the channel
+	testingu.MockTime.Add(25 * time.Hour)
+
+	// channel is not closed, sse connection is still opened
+	select {
+	case <-offsetsChan:
+		t.Fail()
+	default:
+	}
+	require.
+		//
+		vit.N10NUpdate(testProjectionKey, 13)
+	unsubscribe()
+}
