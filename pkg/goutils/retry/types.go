aa614743a190a87f1d73a1f6455c1008783ff990
Denis Gribanov <d.gribanov@dev.untill.com>
2025-08-01 13:34:57 +0300

    use crypto/rand instead of math/rand

--- pkg/goutils/retry/types.go
+++ pkg/goutils/retry/types.go
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2025-present unTill Pro, Ltd. and Contributors
- * @uathor Denis Gribanov
+ * @author Denis Gribanov
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
