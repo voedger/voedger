c16f21d12dab38869bf42679f9ac03e9093580b5
Denis Gribanov <d.gribanov@dev.untill.com>
2025-09-11 12:17:17 +0300

    implement an example for httpu

---
+++ pkg/goutils/httpu/example_test.go
@@ -0,0 +1,36 @@
+package httpu
+
+import (
+    "context"
+    "fmt"
+    "net/http"
+    "net/http/httptest"
+)
+
+func Example() {
+    server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Hello, %s!", r.URL.Query().Get("name"))
+    }))
+    defer server.Close()
+
+    httpClient, cleanup := NewIHTTPClient()
+    defer cleanup()
+
+    resp, err := httpClient.Req(
+        context.Background(),
+        server.URL+"?name=World",
+        "",
+        WithMethod("GET"),
+        WithHeaders("Content-Type", "application/json"),
+    )
+    if err != nil {
+        panic(err)
+    }
+
+    fmt.Println("Response:", resp.Body)
+    fmt.Println("Status:", resp.HTTPResp.StatusCode)
+
+    // Output:
+    // Response: Hello, World!
+    // Status: 200
+}
