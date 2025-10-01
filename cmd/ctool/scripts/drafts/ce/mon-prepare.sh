b3eaa852aff74395499988ab0d6bb64f78d31751
Alisher <38488987+anurmanov@users.noreply.github.com>
2025-10-02 00:52:00 +0500

    perftest-to-ctool: fix voedger ctool (#4184)
    Co-authored-by: Alisher Nurmanov <a.nurmanov@dev.untill.com>

--- cmd/ctool/scripts/drafts/ce/mon-prepare.sh
+++ cmd/ctool/scripts/drafts/ce/mon-prepare.sh
@@ -6,45 +6,27 @@
 
 set -euo pipefail
 
-echo "Setting up monitoring stack on host..."
-echo "Creating monitoring directories on host..."
-sudo mkdir -p /prometheus && mkdir -p ~/prometheus;
-sudo mkdir -p /alertmanager && mkdir -p ~/alertmanager;
-mkdir -p ~/grafana/provisioning/dashboards;
-mkdir -p ~/grafana/provisioning/datasources;
-sudo mkdir -p /var/lib/grafana;
-sudo chown -R 65534:65534 /prometheus;
-sudo chown -R 65534:65534 /alertmanager;
-sudo chown -R 472:472 /var/lib/grafana;
+set -x
 
-echo "Copying monitoring configuration files to host..."
-cp grafana/grafana.ini ~/grafana/grafana.ini
-# Create Grafana datasource configuration
-datasource_config='apiVersion: 1
-datasources:
-  - name: Prometheus
-    type: prometheus
-    access: proxy
-    url: http://prometheus:9090
-    isDefault: true
-    editable: true'
+sudo mkdir -p /prometheus && mkdir -p ~/prometheus
+sudo mkdir -p /alertmanager && mkdir -p ~/alertmanager
+mkdir -p ~/grafana/provisioning/dashboards
+mkdir -p ~/grafana/provisioning/datasources
+sudo mkdir -p /var/lib/grafana
 
-# Copy datasource configuration to host
-echo "$datasource_config" | cat > ~/grafana/provisioning/datasources/datasource.yml
+cp ./grafana/provisioning/dashboards/* ~/grafana/provisioning/dashboards
+envsubst < ./grafana/provisioning/datasources/datasource.yml > ~/grafana/provisioning/datasources/datasource.yml
+cp ./grafana/grafana.ini ~/grafana/grafana.ini
 
-# Copy Grafana dashboard provisioning files
-cp grafana/provisioning/dashboards/dashboards.yml ~/grafana/provisioning/dashboards/dashboards.yml
-cp grafana/provisioning/dashboards/app-processors.json ~/grafana/provisioning/dashboards/app-processors.json
-cp grafana/provisioning/dashboards/node-exporter-full.json ~/grafana/provisioning/dashboards/node-exporter-full.json
-cp grafana/provisioning/dashboards/prometheus.json ~/grafana/provisioning/dashboards/prometheus.json
 
-# Copy Prometheus configuration files
 envsubst < ./prometheus/prometheus.yml > ~/prometheus/prometheus.yml
 
 cp -n ./prometheus/web.yml ~/prometheus/web.yml
-cp prometheus/web.yml ~/prometheus/web.yml
-cp prometheus/alert.rules ~/prometheus/alert.rules
+cp ./prometheus/alert.rules ~/prometheus/alert.rules
+cp ./alertmanager/config.yml ~/alertmanager/config.yml
 
-# Copy Alertmanager configuration
-cp alertmanager/config.yml ~/alertmanager/config.yml
-echo "Monitoring stack setup completed on host."
+sudo chown -R 65534:65534 /prometheus
+sudo chown -R 65534:65534 /alertmanager
+sudo chown -R 472:472 /var/lib/grafana
+
+set +x
