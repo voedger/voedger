f20c6f421ca3be9541f8aa58de3d01c8a83b4972
Denis Gribanov <d.gribanov@dev.untill.com>
2025-10-21 23:37:54 +0300

    federation retry max duration 30 seconds

--- pkg/coreutils/federation/consts.go
+++ pkg/coreutils/federation/consts.go
@@ -7,9 +7,12 @@ package federation
 
 import (
 	"regexp"
+	"time"
 
 	"github.com/voedger/voedger/pkg/iblobstorage"
 )
+
+const federationRetryMaxDuration = 30 * time.Second
 
 var (
 	TemporaryBLOB_URLTTLToDurationLs = map[string]iblobstorage.DurationType{
