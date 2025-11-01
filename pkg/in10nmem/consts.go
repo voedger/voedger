150b77cf04a4b4dfd5a0f1113b46e1047d3cda98
Denis Gribanov <instance6@gmail.com>
2025-11-01 15:26:42 +0300

    [AIR-1990] n10n: implement protection against multiple calls of WatchChannel for the same channel (#4254)

--- pkg/in10nmem/consts.go
+++ pkg/in10nmem/consts.go
@@ -3,7 +3,7 @@
  * Aleksei Ponomarev
  *
  * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree. 
+ * LICENSE file in the root directory of this source tree.
  */
 
 package in10nmem
