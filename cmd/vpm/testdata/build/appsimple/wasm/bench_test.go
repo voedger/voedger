77aea53eee6511dfb17ac99551afe45652e1a59d
Denis Gribanov <d.gribanov@dev.untill.com>
2025-11-18 15:48:49 +0300

    [AIR-2154] voedger: golang 1.25 in subfolders + update deps + use the last voedger

--- cmd/vpm/testdata/build/appsimple/wasm/bench_test.go
+++ cmd/vpm/testdata/build/appsimple/wasm/bench_test.go
@@ -18,6 +18,7 @@ import (
 	"github.com/tetratelabs/wazero"
 	"github.com/tetratelabs/wazero/api"
 	"github.com/tetratelabs/wazero/imports/wasi_snapshot_preview1"
+	"github.com/voedger/voedger/pkg/goutils/strconvu"
 )
 
 func Benchmark_Pbill(b *testing.B) {
--- examples/airs-bp2/untill/go.mod
+++ examples/airs-bp2/untill/go.mod
@@ -1,7 +1,5 @@
 module untill
 
-go 1.24.4
+go 1.25.4
 
-require github.com/voedger/voedger v0.0.0-20240925154640-cd63a37232a6
-
-require github.com/robfig/cron/v3 v3.0.1 // indirect
+require github.com/voedger/voedger v0.0.0-20251118115018-b14c6b2596f2
