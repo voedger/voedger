9d1d18cad76c0a7d9435cbba6226afe6010a3d64
Denis Gribanov <d.gribanov@dev.untill.com>
2026-02-17 16:41:29 +0300

    fix: add comments for noConsumerTimeout and firstResponseWaitWarningInterval

--- pkg/bus/consts.go
+++ pkg/bus/consts.go
@@ -8,6 +8,10 @@ package bus
 import "time"
 
 const (
-	noConsumerTimeout                = 10 * time.Second
+	// how long Write() waits before deciding there is no consumer
+	// e.g. if router is busy writing to the slow http client
+	noConsumerTimeout = 10 * time.Second
+
+	// how often to warn if the first response is not received yet
 	firstResponseWaitWarningInterval = time.Minute
 )
