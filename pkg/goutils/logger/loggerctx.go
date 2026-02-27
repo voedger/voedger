cb9bb40d9a93ebdf31e8609f3b49c2ec4fae7243
Denis Gribanov <d.gribanov@dev.untill.com>
2026-02-27 15:30:16 +0300

    Update copyright information in loggerctx.go

--- pkg/goutils/logger/loggerctx.go
+++ pkg/goutils/logger/loggerctx.go
@@ -1,8 +1,6 @@
 /*
- * Copyright (c) 2020-present unTill Pro, Ltd. and Contributors
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
+ * Copyright (c) 2026-present unTill Software Development Group B.V.
+ * @author Denis Gribanov
  */
 
 package logger
