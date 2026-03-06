6a30e3a65a64454e2159c28cd3465bb0f9ddbe2c
Denis Gribanov <d.gribanov@dev.untill.com>
2026-03-06 11:22:42 +0300

    [AIR-3224] fix data race in TestLogEventAndCUDs (#4373)
    * [AIR-3224] fix data race in TestLogEventAndCUDs
    
    * fix: resolve data race in TestLogEventAndCUDs by adjusting logger context management

--- pkg/processors/command/impl_test.go
+++ pkg/processors/command/impl_test.go
@@ -14,6 +14,7 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"sync"
 	"testing"
 	"time"
 
@@ -832,6 +833,10 @@ func TestLogEventAndCUDs(t *testing.T) {
 	testQName := appdef.NewQName("test", "Article")
 	cudQName := appdef.NewQName(appdef.SysPackage, "CUD")
 
+	var buf syncBuffer
+	logger.SetCtxWriters(&buf, &buf)
+	defer logger.SetCtxWriters(os.Stdout, os.Stderr)
+
 	app := setUp(t, func(wsb appdef.IWorkspaceBuilder, cfg *istructsmem.AppConfigType) {
 		wsb.AddCDoc(testQName).AddField("Name", appdef.DataKind_string, false)
 		wsb.AddCommand(cudQName)
@@ -846,9 +851,7 @@ func TestLogEventAndCUDs(t *testing.T) {
 		require := require.New(t)
 		defer logger.SetLogLevelWithRestore(logger.LogLevelVerbose)()
 
-		var buf bytes.Buffer
-		logger.SetCtxWriters(&buf, &buf)
-		defer logger.SetCtxWriters(os.Stdout, os.Stderr)
+		buf.Reset()
 
 		// insert
 		req := bus.Request{
@@ -888,9 +891,7 @@ func TestLogEventAndCUDs(t *testing.T) {
 		require := require.New(t)
 		defer logger.SetLogLevelWithRestore(logger.LogLevelInfo)()
 
-		var buf bytes.Buffer
-		logger.SetCtxWriters(&buf, &buf)
-		defer logger.SetCtxWriters(os.Stdout, os.Stderr)
+		buf.Reset()
 
 		req := bus.Request{
 			WSID:     1,
@@ -903,4 +904,27 @@ func TestLogEventAndCUDs(t *testing.T) {
 		require.NoError(sysErr)
 		require.NotContains(buf.String(), "evqname")
 	})
+}
+
+type syncBuffer struct {
+	mu  sync.Mutex
+	buf bytes.Buffer
+}
+
+func (sb *syncBuffer) Write(p []byte) (n int, err error) {
+	sb.mu.Lock()
+	defer sb.mu.Unlock()
+	return sb.buf.Write(p)
+}
+
+func (sb *syncBuffer) String() string {
+	sb.mu.Lock()
+	defer sb.mu.Unlock()
+	return sb.buf.String()
+}
+
+func (sb *syncBuffer) Reset() {
+	sb.mu.Lock()
+	defer sb.mu.Unlock()
+	sb.buf.Reset()
 }
