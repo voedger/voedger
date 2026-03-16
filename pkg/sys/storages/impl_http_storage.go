1240b20a7162c38d7796f35a52b153144a3e204a
Denis Gribanov <d.gribanov@dev.untill.com>
2026-03-16 13:54:49 +0300

    [AIR-3322] http storage: return back requests and responses logging (#4402)
    * [AIR-3322] http storage: return back requests and responses logging
    
    * http storage: change requestNumber to atomic.Uint64 for thread safety

--- pkg/sys/storages/impl_http_storage.go
+++ pkg/sys/storages/impl_http_storage.go
@@ -12,14 +12,18 @@ import (
 	"io"
 	"net/http"
 	"strings"
+	"sync/atomic"
 	"time"
 
 	"github.com/voedger/voedger/pkg/appdef"
 	"github.com/voedger/voedger/pkg/goutils/httpu"
+	"github.com/voedger/voedger/pkg/goutils/logger"
 	"github.com/voedger/voedger/pkg/istructs"
 	"github.com/voedger/voedger/pkg/state"
 	"github.com/voedger/voedger/pkg/sys"
 )
+
+var requestNumber atomic.Uint64
 
 type httpStorage struct {
 	httpClient httpu.IHTTPClient
@@ -167,9 +171,19 @@ func (s *httpStorage) Read(key istructs.IStateKeyBuilder, callback istructs.Valu
 		})
 	}
 
+	var reqNumber uint64
+	if logger.IsVerbose() {
+		reqNumber = requestNumber.Add(1)
+		logger.Verbose("req ", reqNumber, ": ", method, " ", kb.url, " body: ", string(kb.body))
+	}
+
 	resp, err := s.httpClient.ReqReader(ctx, kb.url, body, opts...)
 	if err != nil && !errors.Is(err, httpu.ErrUnexpectedStatusCode) {
 		return errorResult(err)
+	}
+
+	if logger.IsVerbose() && reqNumber > 0 { // avoiding case when Verbose level set after request was sent
+		logger.Verbose("resp ", reqNumber, ": ", resp.HTTPResp.StatusCode, " body: ", resp.Body)
 	}
 
 	return callback(nil, &httpValue{
