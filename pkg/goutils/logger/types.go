0b9c179d97fab367cfab9cffdf91df7791c62103
Denis Gribanov <d.gribanov@dev.untill.com>
2026-03-23 14:04:55 +0300

    [AIR-3349] logger: implement log capturing for tests instead of syncBuffer everywhere (#4415)

--- pkg/goutils/logger/types.go
+++ pkg/goutils/logger/types.go
@@ -5,9 +5,36 @@
 
 package logger
 
+import (
+	"bytes"
+	"sync"
+)
+
 type ctxKey struct{}
 
 type logAttrs struct {
 	attrs  map[string]any
 	parent *logAttrs
+}
+
+// copy of testing.TB to avoid importing testing package in production
+type TB interface {
+	Helper()
+	Cleanup(func())
+	Errorf(format string, args ...interface{})
+	FailNow()
+}
+
+type ILogCaptor interface {
+	String() string
+	HasLine(str string, strs ...string)           // fails if no single line contains all substrings (any order)
+	EventuallyHasLine(str string, strs ...string) // same, retries up to 1 second
+	NotContains(str string, strs ...string)       // fails if any substring appears in the log
+	Reset()
+}
+
+type captor struct {
+	mu  sync.Mutex
+	buf bytes.Buffer
+	t   TB
 }
