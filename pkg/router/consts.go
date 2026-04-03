184358dba46dfd928e53a0221a3002a3fcf6bdcf
Denis Gribanov <d.gribanov@dev.untill.com>
2026-04-03 18:28:06 +0300

    [AIR-3515] check remoteaddr as the soruce for IP of the http client (#4450)

--- pkg/router/consts.go
+++ pkg/router/consts.go
@@ -49,7 +49,7 @@ const (
 	hours24                       = 24 * time.Hour
 	DefaultRetryAfterSecondsOn503 = 1
 	logAttrib_Origin              = "origin"
-	logAttrib_Headers             = "headers"
+	logAttrib_RemoteAddr          = "remoteaddr"
 	logAttrib_Projection          = "projection"
 	logAttrib_ChannelID           = "channelid"
 	n10nErrorStage                = "n10n.error"
